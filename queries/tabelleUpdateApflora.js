cb076cace70881dcb48a2099abe6133ea13f94f4
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2015-11-30 15:54:23 +0100

    optimize code

--- queries/tabelleUpdateApflora.js
+++ queries/tabelleUpdateApflora.js
@@ -17,22 +17,24 @@ var mysql = require('mysql'),
   })
 
 module.exports = function (request, callback) {
-  var tabelle = escapeStringForSql(request.params.tabelle),         // der Name der Tabelle, in der die Daten gespeichert werden sollen
-    tabelleIdFeld = escapeStringForSql(request.params.tabelleIdFeld),   // das ist der Name der ID der Tabelle
-    tabelleId = escapeStringForSql(request.params.tabelleId),       // der Wert der ID
-    feld = escapeStringForSql(request.params.feld),            // der Name des Felds, dessen Daten gespeichert werden sollen
-    wert = escapeStringForSql(request.params.wert),            // der Wert, der gespeichert werden soll
-    user = escapeStringForSql(request.params.user),            // der Benutzername
-    date = new Date().toISOString(),                           // wann gespeichert wird
-    table = _.findWhere(config.tables, {tabelleInDb: tabelle}), // Infos über die Tabelle holen
-    mutWannFeld = table.mutWannFeld,                                  // so heisst das Feld für MutWann
-    mutWerFeld = table.mutWerFeld,                                   // so heisst das Feld für MutWer
-    sql
+  const tabelle = escapeStringForSql(request.params.tabelle)       // der Name der Tabelle, in der die Daten gespeichert werden sollen
+  const tabelleIdFeld = escapeStringForSql(request.params.tabelleIdFeld) // der Name der ID der Tabelle
+  const tabelleId = escapeStringForSql(request.params.tabelleId)   // der Wert der ID
+  const feld = escapeStringForSql(request.params.feld)             // der Name des Felds, dessen Daten gespeichert werden sollen
+  const wert = escapeStringForSql(request.params.wert)             // der Wert, der gespeichert werden soll
+  const user = escapeStringForSql(request.params.user)             // der Benutzername
+  const date = new Date().toISOString()                            // wann gespeichert wird
+  const table = _.findWhere(config.tables, {tabelleInDb: tabelle}) // Infos über die Tabelle holen
+  const mutWannFeld = table.mutWannFeld                            // so heisst das Feld für MutWann
+  const mutWerFeld = table.mutWerFeld                              // so heisst das Feld für MutWer
+  let sql
 
-  sql = 'UPDATE ' + tabelle + ' SET ' + feld + '="' + wert + '", ' + mutWannFeld + '="' + date + '", ' + mutWerFeld + '="' + user + '" WHERE ' + tabelleIdFeld + ' = "' + tabelleId + '"'
+  // sql = 'UPDATE ' + tabelle + ' SET ' + feld + '="' + wert + '", ' + mutWannFeld + '="' + date + '", ' + mutWerFeld + '="' + user + '" WHERE ' + tabelleIdFeld + ' = "' + tabelleId + '"'
+  sql = `UPDATE ${tabelle} SET ${feld}="${wert}", ${mutWannFeld}="${date}", ${mutWerFeld}="${user}" WHERE ${tabelleIdFeld} = "${tabelleId}"`
   // Ist ein Feld neu leer, muss NULL übergeben werden. wert ist dann 'undefined'
   if (!wert) {
-    sql = 'UPDATE ' + tabelle + ' SET ' + feld + '= NULL, ' + mutWannFeld + '="' + date + '", ' + mutWerFeld + '="' + user + '" WHERE ' + tabelleIdFeld + ' = "' + tabelleId + '"'
+    // sql = 'UPDATE ' + tabelle + ' SET ' + feld + '= NULL, ' + mutWannFeld + '="' + date + '", ' + mutWerFeld + '="' + user + '" WHERE ' + tabelleIdFeld + ' = "' + tabelleId + '"'
+    sql = `UPDATE ${tabelle} SET ${feld}= NULL, ${mutWannFeld}="${date}", ${mutWerFeld}="${user}" WHERE ${tabelleIdFeld} = "${tabelleId}"`
   }
 
   connection.query(
