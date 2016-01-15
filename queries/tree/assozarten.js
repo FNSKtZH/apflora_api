2175cd975773d4f21f919acfe4901c103ad4df04
Alexander Gabriel <alex@gabriel-software.ch>
2016-01-15 15:05:56 +0100

    convert to es6

--- queries/tree/assozarten.js
+++ queries/tree/assozarten.js
@@ -1,47 +1,42 @@
 'use strict'
 
-var mysql = require('mysql')
-var config = require('../../configuration')
-var escapeStringForSql = require('../escapeStringForSql')
-var connection = mysql.createConnection({
+const mysql = require('mysql')
+const config = require('../../configuration')
+const escapeStringForSql = require('../escapeStringForSql')
+const connection = mysql.createConnection({
   host: 'localhost',
   user: config.db.userName,
   password: config.db.passWord,
   database: 'apflora'
 })
 
-function buildChildFromData (data) {
-  var childrenArray = []
-  var object
-
-  data.forEach(function (assArt) {
-    object = {}
-    object.data = assArt.Artname || '(keine Art gewählt)'
-    object.attr = {
-      id: assArt.AaId,
-      typ: 'assozarten'
+function buildChildrenFromData (data) {
+  return data.map(assArt => {
+    return {
+      data: assArt.Artname || '(keine Art gewählt)',
+      attr: {
+        id: assArt.AaId,
+        typ: 'assozarten'
+      }
     }
-    childrenArray.push(object)
   })
-
-  return childrenArray
 }
 
-module.exports = function (request, reply) {
-  var apId = escapeStringForSql(request.params.apId)
-  var response
+module.exports = (request, reply) => {
+  const apId = escapeStringForSql(request.params.apId)
 
   connection.query(
-    'SELECT AaId, apflora_beob.adb_eigenschaften.Artname FROM assozart LEFT JOIN apflora_beob.adb_eigenschaften ON AaSisfNr = apflora_beob.adb_eigenschaften.TaxonomieId where AaApArtId = ' + apId + ' ORDER BY apflora_beob.adb_eigenschaften.Artname',
-    function (err, data) {
-      if (err) { return reply(err) }
-      response = {}
-      response.data = 'assoziierte Arten (' + data.length + ')'
-      response.attr = {
-        id: 'apOrdnerAssozarten' + apId,
-        typ: 'apOrdnerAssozarten'
+    `SELECT AaId, apflora_beob.adb_eigenschaften.Artname FROM assozart LEFT JOIN apflora_beob.adb_eigenschaften ON AaSisfNr = apflora_beob.adb_eigenschaften.TaxonomieId where AaApArtId = ${apId} ORDER BY apflora_beob.adb_eigenschaften.Artname`,
+    (err, data) => {
+      if (err) return reply(err)
+      const response = {
+        data: 'assoziierte Arten (' + data.length + ')',
+        attr: {
+          id: 'apOrdnerAssozarten' + apId,
+          typ: 'apOrdnerAssozarten'
+        },
+        children: buildChildrenFromData(data)
       }
-      response.children = buildChildFromData(data)
       reply(null, response)
     }
   )
