d563f6d7916d3321415a1380d71eca948e17c321
Alexander Gabriel <alex@gabriel-software.ch>
2016-01-15 15:06:13 +0100

    convert to es6

--- queries/tree/erfkrit.js
+++ queries/tree/erfkrit.js
@@ -1,57 +1,46 @@
 'use strict'
 
-var mysql = require('mysql')
-var config = require('../../configuration')
-var escapeStringForSql = require('../escapeStringForSql')
-var connection = mysql.createConnection({
+const mysql = require('mysql')
+const config = require('../../configuration')
+const escapeStringForSql = require('../escapeStringForSql')
+const connection = mysql.createConnection({
   host: 'localhost',
   user: config.db.userName,
   password: config.db.passWord,
   database: 'apflora'
 })
 
-function buildChildFromData (data) {
-  var childrenArray = []
-  var object
-  var beschriftung
-  var beurteilText
-  var erfkritText
+const buildChildrenFromData = (data) => {
+  return data.map(erfkrit => {
+    const beurteilText = erfkrit.BeurteilTxt || '(keine Beurteilung)'
+    const erfkritText = erfkrit.ErfkritTxt || '(kein Kriterium)'
+    const beschriftung = `${beurteilText}: ${erfkritText}`
 
-  data.forEach(function (erfkrit) {
-    beurteilText = erfkrit.BeurteilTxt || '(keine Beurteilung)'
-    erfkritText = erfkrit.ErfkritTxt || '(kein Kriterium)'
-    beschriftung = beurteilText + ': ' + erfkritText
-
-    object = {}
-    object.data = beschriftung
-
-    // erfkrit voransetzen, damit die ID im ganzen Baum eindeutig ist
-    object.attr = {
-      id: erfkrit.ErfkritId,
-      typ: 'erfkrit'
+    return {
+      data: beschriftung,
+      attr: {
+        id: erfkrit.ErfkritId,
+        typ: 'erfkrit'
+      }
     }
-    childrenArray.push(object)
   })
-
-  return childrenArray
 }
 
-module.exports = function (request, reply) {
-  var apId = escapeStringForSql(request.params.apId)
+module.exports = (request, reply) => {
+  const apId = escapeStringForSql(request.params.apId)
 
   connection.query(
-    'SELECT ErfkritId, ApArtId, BeurteilTxt, ErfkritTxt, BeurteilOrd FROM erfkrit LEFT JOIN ap_erfkrit_werte ON ErfkritErreichungsgrad = BeurteilId where ApArtId = ' + apId + ' ORDER BY BeurteilOrd',
-    function (err, data) {
-      var node = {}
-
-      if (err) { return reply(err) }
-
-      node.data = 'AP-Erfolgskriterien (' + data.length + ')'
-      node.attr = {
-        id: 'apOrdnerErfkrit' + apId,
-        typ: 'apOrdnerErfkrit'
+    `SELECT ErfkritId, ApArtId, BeurteilTxt, ErfkritTxt, BeurteilOrd FROM erfkrit LEFT JOIN ap_erfkrit_werte ON ErfkritErreichungsgrad = BeurteilId where ApArtId = ${apId} ORDER BY BeurteilOrd`,
+    (err, data) => {
+      if (err) return reply(err)
+      const node = {
+        data: `AP-Erfolgskriterien (${data.length})`,
+        attr: {
+          id: `apOrdnerErfkrit${apId}`,
+          typ: 'apOrdnerErfkrit'
+        },
+        children: buildChildrenFromData(data)
       }
-      node.children = buildChildFromData(data)
 
       reply(null, node)
     }
