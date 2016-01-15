890637cde35501f2f21bf0b4f14acf394b93c189
Alexander Gabriel <alex@gabriel-software.ch>
2016-01-15 15:06:05 +0100

    convert to es6

--- queries/tree/beobNichtZuzuordnen.js
+++ queries/tree/beobNichtZuzuordnen.js
@@ -1,58 +1,47 @@
 'use strict'
 
-var mysql = require('mysql')
-var config = require('../../configuration')
-var escapeStringForSql = require('../escapeStringForSql')
-var connection = mysql.createConnection({
+const mysql = require('mysql')
+const config = require('../../configuration')
+const escapeStringForSql = require('../escapeStringForSql')
+const connection = mysql.createConnection({
   host: 'localhost',
   user: config.db.userName,
   password: config.db.passWord,
   database: 'apflora_beob'
 })
 
-function buildChildFromData (data) {
-  var childrenArray = []
-  var object
+function buildChildrenFromData (data) {
+  return data.map(beob => {
+    const datum = beob.Datum || '(kein Datum)'
+    const autor = beob.Autor || '(kein Autor)'
 
-  data.forEach(function (beob) {
-    object = {}
-    var datum = beob.Datum || '(kein Datum)'
-    var autor = beob.Autor || '(kein Autor)'
-
-    object.data = datum + ': ' + autor
-    // beob voransetzen, damit die ID im ganzen Baum eindeutig ist
-    object.attr = {
-      id: 'beob' + beob.NO_NOTE,
-      typ: 'beobNichtZuzuordnen',
-      beobtyp: beob.beobtyp
+    return {
+      data: `${datum}: ${autor}`,
+      attr: {
+        // beob voransetzen, damit die ID im ganzen Baum eindeutig ist
+        id: `beob${beob.NO_NOTE}`,
+        typ: `beobNichtZuzuordnen`,
+        beobtyp: beob.beobtyp
+      }
     }
-    childrenArray.push(object)
   })
-
-  return childrenArray
 }
 
-module.exports = function (request, reply) {
-  var apId = escapeStringForSql(request.params.apId)
+module.exports = (request, reply) => {
+  const apId = escapeStringForSql(request.params.apId)
 
   connection.query(
-    'SELECT apflora_beob.beob_bereitgestellt.NO_ISFS, apflora.beobzuordnung.NO_NOTE, apflora.beobzuordnung.beobNichtZuordnen, apflora.beobzuordnung.BeobBemerkungen, apflora.beobzuordnung.BeobMutWann, apflora.beobzuordnung.BeobMutWer, apflora_beob.beob_bereitgestellt.Datum, apflora_beob.beob_bereitgestellt.Autor, "infospezies" AS beobtyp FROM apflora.beobzuordnung INNER JOIN apflora_beob.beob_bereitgestellt ON apflora.beobzuordnung.NO_NOTE = apflora_beob.beob_bereitgestellt.NO_NOTE WHERE apflora.beobzuordnung.NO_NOTE IS NOT NULL AND apflora.beobzuordnung.beobNichtZuordnen=1 AND apflora_beob.beob_bereitgestellt.NO_ISFS=' + apId + ' UNION SELECT apflora_beob.beob_bereitgestellt.NO_ISFS, apflora.beobzuordnung.NO_NOTE, apflora.beobzuordnung.beobNichtZuordnen, apflora.beobzuordnung.BeobBemerkungen, apflora.beobzuordnung.BeobMutWann, apflora.beobzuordnung.BeobMutWer, apflora_beob.beob_bereitgestellt.Datum, apflora_beob.beob_bereitgestellt.Autor, "evab" AS beobtyp FROM apflora.beobzuordnung INNER JOIN apflora_beob.beob_bereitgestellt ON apflora.beobzuordnung.NO_NOTE = apflora_beob.beob_bereitgestellt.NO_NOTE_PROJET WHERE apflora.beobzuordnung.NO_NOTE IS NOT NULL AND apflora.beobzuordnung.beobNichtZuordnen=1 AND apflora_beob.beob_bereitgestellt.NO_ISFS=' + apId + ' ORDER BY Datum DESC LIMIT 100',
-    function (err, data) {
-      var node = {}
-
-      if (err) { return reply(err) }
-
-      if (data.length < 100) {
-        node.data = 'nicht zuzuordnende Beobachtungen (' + data.length + ')'
-      } else {
-        node.data = 'nicht zuzuordnende Beobachtungen (neuste ' + data.length + ')'
+    `SELECT apflora_beob.beob_bereitgestellt.NO_ISFS, apflora.beobzuordnung.NO_NOTE, apflora.beobzuordnung.beobNichtZuordnen, apflora.beobzuordnung.BeobBemerkungen, apflora.beobzuordnung.BeobMutWann, apflora.beobzuordnung.BeobMutWer, apflora_beob.beob_bereitgestellt.Datum, apflora_beob.beob_bereitgestellt.Autor, "infospezies" AS beobtyp FROM apflora.beobzuordnung INNER JOIN apflora_beob.beob_bereitgestellt ON apflora.beobzuordnung.NO_NOTE = apflora_beob.beob_bereitgestellt.NO_NOTE WHERE apflora.beobzuordnung.NO_NOTE IS NOT NULL AND apflora.beobzuordnung.beobNichtZuordnen=1 AND apflora_beob.beob_bereitgestellt.NO_ISFS = ${apId} UNION SELECT apflora_beob.beob_bereitgestellt.NO_ISFS, apflora.beobzuordnung.NO_NOTE, apflora.beobzuordnung.beobNichtZuordnen, apflora.beobzuordnung.BeobBemerkungen, apflora.beobzuordnung.BeobMutWann, apflora.beobzuordnung.BeobMutWer, apflora_beob.beob_bereitgestellt.Datum, apflora_beob.beob_bereitgestellt.Autor, "evab" AS beobtyp FROM apflora.beobzuordnung INNER JOIN apflora_beob.beob_bereitgestellt ON apflora.beobzuordnung.NO_NOTE = apflora_beob.beob_bereitgestellt.NO_NOTE_PROJET WHERE apflora.beobzuordnung.NO_NOTE IS NOT NULL AND apflora.beobzuordnung.beobNichtZuordnen=1 AND apflora_beob.beob_bereitgestellt.NO_ISFS = ${apId} ORDER BY Datum DESC LIMIT 100`,
+    (err, data) => {
+      if (err) return reply(err)
+      const node = {
+        data: `nicht zuzuordnende Beobachtungen (${data.length < 100 ? '' : 'neuste '}${data.length})`,
+        attr: {
+          id: 'apOrdnerBeobNichtZuzuordnen' + apId,
+          typ: 'apOrdnerBeobNichtZuzuordnen'
+        },
+        children: buildChildrenFromData(data)
       }
-
-      node.attr = {
-        id: 'apOrdnerBeobNichtZuzuordnen' + apId,
-        typ: 'apOrdnerBeobNichtZuzuordnen'
-      }
-      node.children = buildChildFromData(data)
 
       reply(null, node)
     }
