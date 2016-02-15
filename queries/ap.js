42a0fa3e308841028399e104f75addcdac216cba
Alexander Gabriel <alex.barbalex@gmail.com>
2016-02-15 17:42:23 +0100

    convert to pg

--- queries/ap.js
+++ queries/ap.js
@@ -1,31 +1,39 @@
 'use strict'
 
-const mysql = require('mysql')
+const pg = require('pg')
 const config = require('../configuration')
+const connectionString = config.pg.connectionString
 const escapeStringForSql = require('./escapeStringForSql')
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: config.db.userName,
-  password: config.db.passWord,
-  database: 'apflora'
-})
 
 module.exports = (request, callback) => {
   const apId = escapeStringForSql(request.params.apId)
 
-  connection.query(`
-    SELECT
-      apflora.ap.ApArtId,
-      apflora_beob.adb_eigenschaften.Artname,
-      apflora.ap.ApStatus,
-      apflora.ap.ApJahr,
-      apflora.ap.ApUmsetzung,
-      apflora.ap.ApBearb,
-      apflora.ap.ApArtwert,
-      apflora.ap.MutWann, apflora.ap.MutWer
-    FROM apflora.ap
-      INNER JOIN apflora_beob.adb_eigenschaften ON apflora.ap.ApArtId = apflora_beob.adb_eigenschaften.TaxonomieId
-    WHERE ApArtId = ${apId}`,
-    (err, data) => callback(err, data)
-  )
+  // get a pg client from the connection pool
+  pg.connect(connectionString, (error, apfDb, done) => {
+    if (error) {
+      if (apfDb) done(apfDb)
+      console.log('an error occured when trying to connect to db apflora')
+    }
+    const sql = `
+      SELECT
+        apflora.ap."ApArtId",
+        beob.adb_eigenschaften."Artname",
+        apflora.ap."ApStatus",
+        apflora.ap."ApJahr",
+        apflora.ap."ApUmsetzung",
+        apflora.ap."ApBearb",
+        apflora.ap."ApArtwert",
+        apflora.ap."MutWann",
+        apflora.ap."MutWer"
+      FROM
+        apflora.ap
+        INNER JOIN
+          beob.adb_eigenschaften
+          ON apflora.ap."ApArtId" = beob.adb_eigenschaften."TaxonomieId"
+      WHERE
+        apflora.ap."ApArtId" = ${apId}`
+    apfDb.query(sql, (error, result) => {
+      callback(error, result.rows)
+    })
+  })
 }
--- queries/apInsert.js
+++ queries/apInsert.js
@@ -1,54 +1,62 @@
 'use strict'
 
-const mysql = require('mysql')
+/*
+ * need to test this with postgresql
+ */
+
+const pg = require('pg')
 const config = require('../configuration')
+const connectionString = config.pg.connectionString
 const escapeStringForSql = require('./escapeStringForSql')
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: config.db.userName,
-  password: config.db.passWord,
-  database: 'apflora'
-})
-const connection2 = mysql.createConnection({
-  host: 'localhost',
-  user: config.db.userName,
-  password: config.db.passWord,
-  database: 'apflora_beob'
-})
 
 module.exports = (request, callback) => {
   const apId = escapeStringForSql(request.params.apId)
   const user = escapeStringForSql(request.params.user)
   const date = new Date().toISOString()
 
-  // neuen AP einfügen
-  connection.query(`
-    INSERT INTO apflora.ap (ApArtId, MutWann, MutWer)
-    VALUES (${apId}, "${date}", "${user}")`,
-    (err, data) => {
-      if (err) callback(err, null)
-      // Artwert holen
-      connection2.query(`
-        SELECT Artwert
-        FROM apflora_beob.adb_eigenschaften
-        WHERE TaxonomieId = ${apId}`,
-        (err, data) => {
-          // keine Fehler melden, wenn bloss der Artwert nicht geholt wurde
-          if (data && data[0]) {
-            const artwert = data[0]
-            if (artwert) {
-              connection.query(`
-                UPDATE apflora.ap
-                SET ApArtwert = "${artwert}"
-                WHERE ApArtId = ${apId}`,
-                (err, data) => callback(err, apId)
-              )
-            }
-          } else {
-            callback(err, null)
-          }
-        }
-      )
+  // get a pg client from the connection pool
+  pg.connect(connectionString, (error, apfDb, done) => {
+    if (error) {
+      if (apfDb) done(apfDb)
+      console.log('an error occured when trying to connect to db apflora')
     }
-  )
+    // neuen AP einfügen
+    apfDb.query(`
+      INSERT INTO
+        apflora.ap ("ApArtId", "MutWann", "MutWer")
+      VALUES
+        (${apId}, '${date}', '${user}')`,
+      (err, data) => {
+        if (err) callback(err, null)
+        // Artwert holen
+        apfDb.query(`
+          SELECT
+            "Artwert"
+          FROM
+            apflora_beob.adb_eigenschaften
+          WHERE
+            "TaxonomieId" = ${apId}`,
+          (err, data) => {
+            // keine Fehler melden, wenn bloss der Artwert nicht geholt wurde
+            if (data && data[0]) {
+              const artwert = data[0]
+              if (artwert) {
+                apfDb.query(`
+                  UPDATE
+                    apflora.ap
+                  SET
+                    "ApArtwert" = '${artwert}'
+                  WHERE
+                    "ApArtId" = ${apId}`,
+                  (err, data) => callback(err, apId)
+                )
+              }
+            } else {
+              callback(err, null)
+            }
+          }
+        )
+      }
+    )
+  })
 }
