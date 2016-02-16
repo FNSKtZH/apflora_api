50e13624b7dc61e9250a2e56bbdcd5d2804a5fb8
Alexander Gabriel <alex.barbalex@gmail.com>
2016-02-16 14:46:54 +0100

    convert to pg

--- queries/beobDistzutpopInfospezies.js
+++ queries/beobDistzutpopInfospezies.js
@@ -16,30 +16,30 @@ module.exports = (request, callback) => {
     }
     const sql = `
       SELECT
-        beob.beob_infospezies.NO_NOTE,
-        beob.beob_infospezies.NO_ISFS,
-        apflora.tpop.TPopId,
-        beob.beob_infospezies.FNS_XGIS,
-        beob.beob_infospezies.FNS_YGIS,
-        TPopXKoord,
-        TPopYKoord,
+        beob.beob_infospezies."NO_NOTE",
+        beob.beob_infospezies."NO_ISFS",
+        apflora.tpop."TPopId",
+        beob.beob_infospezies."FNS_XGIS",
+        beob.beob_infospezies."FNS_YGIS",
+        apflora.tpop.TPopXKoord,
+        apflora.tpop.TPopYKoord,
         PopNr,
-        TPopNr,
-        TPopFlurname,
+        apflora.tpop.TPopNr,
+        apflora.tpop.TPopFlurname,
         SQRT(
-          power((beob.beob_infospezies.FNS_XGIS - TPopXKoord), 2) +
-          power((beob.beob_infospezies.FNS_YGIS - TPopYKoord), 2)
+          power((beob.beob_infospezies.FNS_XGIS - apflora.tpop.TPopXKoord), 2) +
+          power((beob.beob_infospezies.FNS_YGIS - apflora.tpop.TPopYKoord), 2)
         ) AS DistZuTPop
       FROM beob.beob_infospezies
         INNER JOIN (apflora.pop INNER JOIN apflora.tpop ON apflora.pop.PopId = apflora.tpop.PopId) ON NO_ISFS = ApArtId
       WHERE NO_NOTE = "${beobId}"
-        AND TPopXKoord IS NOT NULL
-        AND TPopYKoord IS NOT NULL
+        AND apflora.tpop.TPopXKoord IS NOT NULL
+        AND apflora.tpop.TPopYKoord IS NOT NULL
         AND FNS_XGIS IS NOT NULL
         AND FNS_YGIS IS NOT NULL
       ORDER BY
         DistzuTPop,
-        TPopFlurname`
+        apflora.tpop.TPopFlurname`
     apfDb.query(sql, (error, result) => {
       callback(error, result.rows)
     })
