23979b3f263229ae9255167f6ea6f98cab899561
Alexander Gabriel <alex.barbalex@gmail.com>
2016-02-19 14:49:26 +0100

    convert to pg

--- queries/beobKarte.js
+++ queries/beobKarte.js
@@ -65,46 +65,63 @@ module.exports = (request, callback) => {
     // tpopId wurde übergeben > auf tpop filtern
     sql = `
     SELECT
-      beob.beob_infospezies.NO_NOTE,
-      beob.beob_infospezies.NO_ISFS,
-      beob.beob_infospezies.FNS_XGIS AS X,
-      beob.beob_infospezies.FNS_YGIS AS Y,
-      beob.beob_infospezies.A_NOTE,
-      beob.beob_bereitgestellt.Datum AS Datum,
-      beob.beob_bereitgestellt.Autor,
-      beob.beob_infospezies.PROJET,
-      beob.beob_infospezies.DESC_LOCALITE,
-      apflora.beobzuordnung.TPopId,
-      apflora.tpop.TPopXKoord,
-      apflora.tpop.TPopYKoord
-    FROM (beob.beob_infospezies
-      INNER JOIN beob.beob_bereitgestellt ON beob.beob_infospezies.NO_NOTE = beob.beob_bereitgestellt.NO_NOTE)
-      INNER JOIN (apflora.tpop
-        INNER JOIN apflora.beobzuordnung ON apflora.tpop.TPopId = apflora.beobzuordnung.TPopId) ON beob.beob_infospezies.NO_NOTE = apflora.beobzuordnung.NO_NOTE
-    WHERE beob.beob_infospezies.FNS_XGIS > 0
-      AND beob.beob_infospezies.FNS_YGIS > 0
-      AND apflora.beobzuordnung.TPopId = ${tpopId}
+      beob.beob_infospezies."NO_NOTE",
+      beob.beob_infospezies."NO_ISFS",
+      beob.beob_infospezies."FNS_XGIS" AS "X",
+      beob.beob_infospezies."FNS_YGIS" AS "Y",
+      beob.beob_infospezies."A_NOTE",
+      beob.beob_bereitgestellt."Datum" AS "Datum",
+      beob.beob_bereitgestellt."Autor",
+      beob.beob_infospezies."PROJET",
+      beob.beob_infospezies."DESC_LOCALITE",
+      apflora.beobzuordnung."TPopId",
+      apflora.tpop."TPopXKoord",
+      apflora.tpop."TPopYKoord"
+    FROM
+      (beob.beob_infospezies
+      INNER JOIN
+        beob.beob_bereitgestellt
+        ON beob.beob_infospezies."NO_NOTE" = beob.beob_bereitgestellt."NO_NOTE")
+      INNER JOIN
+        (apflora.tpop
+        INNER JOIN
+          apflora.beobzuordnung
+          ON apflora.tpop."TPopId" = apflora.beobzuordnung."TPopId")
+        ON beob.beob_infospezies."NO_NOTE" = apflora.beobzuordnung."NO_NOTE"
+    WHERE
+      beob.beob_infospezies."FNS_XGIS" > 0
+      AND beob.beob_infospezies."FNS_YGIS" > 0
+      AND apflora.beobzuordnung."TPopId" = ${tpopId}
     UNION SELECT
-      beob.beob_evab.NO_NOTE_PROJET AS NO_NOTE,
-      beob.beob_evab.NO_ISFS,
-      beob.beob_evab.COORDONNEE_FED_E AS X,
-      beob.beob_evab.COORDONNEE_FED_N AS Y,
-      beob.beob_evab.A_NOTE,
-      beob.beob_bereitgestellt.Datum AS Datum,
-      beob.beob_bereitgestellt.Autor,
-      beob.beob_evab.Projekt_ZH AS PROJET,
-      beob.beob_evab.DESC_LOCALITE_ AS DESC_LOCALITE,
-      apflora.beobzuordnung.TPopId,
-      apflora.tpop.TPopXKoord,
-      apflora.tpop.TPopYKoord
-    FROM (beob.beob_bereitgestellt
-      INNER JOIN beob.beob_evab ON beob.beob_bereitgestellt.NO_NOTE_PROJET = beob.beob_evab.NO_NOTE_PROJET)
-      INNER JOIN (apflora.tpop
-        INNER JOIN apflora.beobzuordnung ON apflora.tpop.TPopId = apflora.beobzuordnung.TPopId) ON beob.beob_evab.NO_NOTE_PROJET = apflora.beobzuordnung.NO_NOTE
-    WHERE beob.beob_evab.COORDONNEE_FED_E > 0
-      AND beob.beob_evab.COORDONNEE_FED_N > 0
-      AND apflora.beobzuordnung.TPopId = ${tpopId}
-    ORDER BY Datum DESC
+      beob.beob_evab."NO_NOTE_PROJET" AS "NO_NOTE",
+      beob.beob_evab."NO_ISFS",
+      beob.beob_evab."COORDONNEE_FED_E" AS "X",
+      beob.beob_evab."COORDONNEE_FED_N" AS "Y",
+      beob.beob_evab."A_NOTE",
+      beob.beob_bereitgestellt."Datum" AS "Datum",
+      beob.beob_bereitgestellt."Autor",
+      beob.beob_evab."Projekt_ZH" AS "PROJET",
+      beob.beob_evab."DESC_LOCALITE_" AS "DESC_LOCALITE",
+      apflora.beobzuordnung."TPopId",
+      apflora.tpop."TPopXKoord",
+      apflora.tpop."TPopYKoord"
+    FROM
+      (beob.beob_bereitgestellt
+      INNER JOIN
+        beob.beob_evab
+        ON beob.beob_bereitgestellt."NO_NOTE_PROJET" = beob.beob_evab."NO_NOTE_PROJET")
+      INNER JOIN
+        (apflora.tpop
+        INNER JOIN
+          apflora.beobzuordnung
+          ON apflora.tpop."TPopId" = apflora.beobzuordnung."TPopId")
+        ON beob.beob_evab."NO_NOTE_PROJET" = apflora.beobzuordnung."NO_NOTE"
+    WHERE
+      beob.beob_evab."COORDONNEE_FED_E" > 0
+      AND beob.beob_evab."COORDONNEE_FED_N" > 0
+      AND apflora.beobzuordnung."TPopId" = ${tpopId}
+    ORDER BY
+      "Datum" DESC
     LIMIT 100`
   } else if (apId) {
     // apart_id wurde übergeben > auf Art filtern
