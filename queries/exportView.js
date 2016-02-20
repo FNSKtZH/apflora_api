9e8ccfe02d1d4102ff1d5a1f1adc0ea4f658b623
Alexander Gabriel <alex.barbalex@gmail.com>
2016-02-20 18:05:18 +0100

    convert to pg

--- queries/exportView.js
+++ queries/exportView.js
@@ -1,30 +1,29 @@
 'use strict'
 
-const mysql = require('mysql')
+const pg = require('pg')
 const config = require('../configuration')
+const connectionString = config.pg.connectionString
 const escapeStringForSql = require('./escapeStringForSql')
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: config.db.userName,
-  password: config.db.passWord,
-  database: 'apflora_views'
-})
 
 module.exports = (request, callback) => {
   const view = escapeStringForSql(request.params.view) // Name des Views, aus dem die Daten geholt werden sollen
   const apId = escapeStringForSql(request.params.apId)
   const sql = apId ? `SELECT * FROM ${view} WHERE ApArtId = ${apId}` : `SELECT * FROM ${view}`
 
-  connection.query(
-    sql,
-    (err, data) => {
+  // get a pg client from the connection pool
+  pg.connect(connectionString, (error, apfDb, done) => {
+    if (error) {
+      if (apfDb) done(apfDb)
+      console.log('an error occured when trying to connect to db apflora')
+    }
+    apfDb.query(sql, (error, data) => {
       // null-werte eliminieren
-      data.forEach(object => {
+      data.forEach((object) => {
         Object.keys(object).forEach((key) => {
           if (object[key] === null) object[key] = ''
         })
       })
-      callback(err, data)
-    }
-  )
+      callback(error, data)
+    })
+  })
 }
