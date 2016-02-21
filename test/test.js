38e4051b8a2aaf29f53e3a5420dd2877ebd0e2c6
Alexander Gabriel <alex.barbalex@gmail.com>
2016-02-21 18:16:27 +0100

    add tests

--- test/test.js
+++ test/test.js
@@ -42,3 +42,39 @@ global.describe('/apKarte/apId=900', () => {
       })
   })
 })
+
+global.describe('/apliste/programm=programmAp', () => {
+  global.it('should return more than 50 rows', (done) => {
+    server
+      .get('/apliste/programm=programmAp')
+      .end((err, res) => {
+        if (err) throw err
+        res.body.length.should.be.above(50)
+        done()
+      })
+  })
+})
+
+global.describe('/apliste/programm=programmNeu', () => {
+  global.it('should return more than 7400 rows', (done) => {
+    server
+      .get('/apliste/programm=programmNeu')
+      .end((err, res) => {
+        if (err) throw err
+        res.body.length.should.be.above(7400)
+        done()
+      })
+  })
+})
+
+global.describe('/apliste/programm=programmAlle', () => {
+  global.it('should return more than 520 rows', (done) => {
+    server
+      .get('/apliste/programm=programmAlle')
+      .end((err, res) => {
+        if (err) throw err
+        res.body.length.should.be.above(520)
+        done()
+      })
+  })
+})
