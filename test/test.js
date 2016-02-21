0e1f095a69a501df57b7d4cea70b3160e0883b2d
Alexander Gabriel <alex.barbalex@gmail.com>
2016-02-21 18:00:06 +0100

    add tests

--- test/test.js
+++ test/test.js
@@ -5,12 +5,39 @@ const should = require('should')
 
 const server = supertest.agent('http://0.0.0.0:4001')
 
-describe('/adressen', () => {
-  it('should return more than 140 rows', (done) => {
+global.describe('/adressen', () => {
+  global.it('should return more than 140 rows', (done) => {
     server
       .get('/adressen')
       .end((err, res) => {
+        if (err) throw err
         res.body.length.should.be.above(140)
+        done()
+      })
+  })
+})
+
+global.describe('/ap=900', () => {
+  global.it('should return 1 row with ApArtId 900', (done) => {
+    server
+      .get('/ap=900')
+      .end((err, res) => {
+        if (err) throw err
+        res.body.length.should.equal(1)
+        res.body[0].ApArtId.should.equal(900)
+        done()
+      })
+  })
+})
+
+global.describe('/apKarte/apId=900', () => {
+  global.it('should return more than 100 rows with ApArtId 900', (done) => {
+    server
+      .get('/apKarte/apId=900')
+      .end((err, res) => {
+        if (err) throw err
+        res.body.length.should.be.above(100)
+        res.body[0].ApArtId.should.equal(900)
         done()
       })
   })
