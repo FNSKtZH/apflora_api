3342437228f3c3cd32eee93991c1c4254f8f62ce
Alexander Gabriel <alex.barbalex@gmail.com>
2016-02-22 13:00:33 +0100

    test anmeldung

--- test/test.js
+++ test/test.js
@@ -2,6 +2,7 @@
 
 const supertest = require('supertest')
 const should = require('should')
+const appPassFile = require('../appPass.json')
 
 const server = supertest.agent('http://0.0.0.0:4001')
 
@@ -17,6 +18,7 @@ global.describe('/adressen', () => {
   })
 })
 
+/* dont test every time because of length
 global.describe('/aktualisiereArteigenschaften', function () {
   // takes about 33 seconds!
   this.timeout(100000)
@@ -27,6 +29,31 @@ global.describe('/aktualisiereArteigenschaften', function () {
         if (err) throw err
         res.status.should.equal(200)
         res.error.should.equal(false)
+        done()
+      })
+  })
+})*/
+
+global.describe('/anmeldung', () => {
+  global.it('should accept known user', (done) => {
+    const name = appPassFile.user
+    const pwd = appPassFile.pass
+    server
+      .get(`/anmeldung/name=${name}/pwd=${pwd}`)
+      .end((err, res) => {
+        if (err) throw err
+        res.status.should.equal(200)
+        res.body.length.should.equal(1)
+        done()
+      })
+  })
+  global.it('should not accept unknown user', (done) => {
+    server
+      .get('/anmeldung/name=whoami/pwd=dontknow')
+      .end((err, res) => {
+        if (err) throw err
+        res.status.should.equal(200)
+        res.body.length.should.equal(0)
         done()
       })
   })
