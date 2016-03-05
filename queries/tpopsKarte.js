b8ab1653e35fb13edeab84aa5db30a7714fbfe61
Alexander Gabriel <alex@gabriel-software.ch>
2016-03-05 16:18:38 +0100

    convert to pg

--- queries/tpopsKarte.js
+++ queries/tpopsKarte.js
@@ -1,40 +1,52 @@
 'use strict'
 
-const pg = require('pg')
-const config = require('../configuration')
-const connectionString = config.pg.connectionString
+const escapeStringForSql = require('./escapeStringForSql')
 
 module.exports = (request, callback) => {
   const popId = escapeStringForSql(request.params.popId)
 
   // Daten abfragen
-  connection.query(`
+  request.pg.client.query(`
     SELECT
-      ap.ApArtId,
-      beob.adb_eigenschaften.Artname,
-      ap_umsetzung_werte.DomainTxt AS ApUmsetzung,
-      pop.PopId,
-      pop.PopNr,
-      pop.PopName,
-      pop_status_werte.HerkunftTxt AS PopHerkunft,
-      pop.PopBekanntSeit,
-      tpop.TPopId,
-      tpop.TPopFlurname,
-      tpop.TPopNr,
-      tpop.TPopGemeinde,
-      tpop.TPopXKoord,
-      tpop.TPopYKoord,
-      domPopHerkunft_1.HerkunftTxt AS TPopHerkunft
-    FROM (((((ap
-      INNER JOIN pop ON ap.ApArtId = pop.ApArtId)
-      INNER JOIN tpop ON pop.PopId = tpop.PopId)
-      INNER JOIN beob.adb_eigenschaften ON ap.ApArtId = beob.adb_eigenschaften.TaxonomieId)
-      LEFT JOIN pop_status_werte ON pop.PopHerkunft = pop_status_werte.HerkunftId)
-      LEFT JOIN ap_umsetzung_werte ON ap.ApUmsetzung = ap_umsetzung_werte.DomainCode)
-      LEFT JOIN pop_status_werte AS domPopHerkunft_1 ON tpop.TPopHerkunft = domPopHerkunft_1.HerkunftId
-    WHERE tpop.TPopXKoord Is Not Null
-      AND tpop.TPopYKoord Is Not Null
-      AND pop.PopId = ${popId}`,
-    (err, data) => callback(err, data)
+      apflora.ap."ApArtId",
+      beob.adb_eigenschaften."Artname",
+      apflora.ap_umsetzung_werte."DomainTxt" AS "ApUmsetzung",
+      apflora.pop."PopId",
+      apflora.pop."PopNr",
+      apflora.pop."PopName",
+      apflora.pop_status_werte."HerkunftTxt" AS "PopHerkunft",
+      apflora.pop."PopBekanntSeit",
+      apflora.tpop."TPopId",
+      apflora.tpop."TPopFlurname",
+      apflora.tpop."TPopNr",
+      apflora.tpop."TPopGemeinde",
+      apflora.tpop."TPopXKoord",
+      apflora.tpop."TPopYKoord",
+      "domPopHerkunft_1"."HerkunftTxt" AS "TPopHerkunft"
+    FROM
+      (((((apflora.ap
+      INNER JOIN
+        apflora.pop
+        ON apflora.ap."ApArtId" = apflora.pop."ApArtId")
+      INNER JOIN
+        apflora.tpop
+        ON apflora.pop."PopId" = apflora.tpop."PopId")
+      INNER JOIN
+        beob.adb_eigenschaften
+        ON apflora.ap."ApArtId" = beob.adb_eigenschaften."TaxonomieId")
+      LEFT JOIN
+        apflora.pop_status_werte
+        ON apflora.pop."PopHerkunft" = apflora.pop_status_werte."HerkunftId")
+      LEFT JOIN
+        apflora.ap_umsetzung_werte
+        ON apflora.ap."ApUmsetzung" = apflora.ap_umsetzung_werte."DomainCode")
+      LEFT JOIN
+        apflora.pop_status_werte AS "domPopHerkunft_1"
+        ON apflora.tpop."TPopHerkunft" = "domPopHerkunft_1"."HerkunftId"
+    WHERE
+      apflora.tpop."TPopXKoord" Is Not Null
+      AND apflora.tpop."TPopYKoord" Is Not Null
+      AND apflora.pop."PopId" = ${popId}`,
+    (err, data) => callback(err, data.rows)
   )
 }
