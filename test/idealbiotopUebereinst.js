fc7ffd648797f0da6318f5c71a9ba928ddf02ca5
Alexander Gabriel <alex.barbalex@gmail.com>
2016-03-05 20:19:16 +0100

    test idealbiotopUebereinst

---
+++ test/idealbiotopUebereinst.js
@@ -0,0 +1,31 @@
+'use strict'
+
+// Load modules
+
+const Code = require('code')
+const Lab = require('lab')
+
+// test shortcuts
+
+const lab = exports.lab = Lab.script()
+const describe = lab.describe
+const it = lab.it
+const expect = Code.expect
+
+// start server
+
+const server = require('../server.js')
+
+// test
+
+describe('/idealbiotopUebereinst', () => {
+  it('should get 5 rows', (done) => {
+    const method = 'GET'
+    const url = '/idealbiotopUebereinst'
+    server.inject({ method, url }, (res) => {
+      expect(res.statusCode).to.equal(200)
+      expect(res.result.length).to.equal(5)
+      done()
+    })
+  })
+})
