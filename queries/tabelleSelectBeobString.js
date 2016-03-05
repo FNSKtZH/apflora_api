9e580e061e7313aabb35076e55388a949098ff60
Alexander Gabriel <alex@gabriel-software.ch>
2016-03-05 10:46:00 +0100

    convert to pg

--- queries/tabelleSelectBeobString.js
+++ queries/tabelleSelectBeobString.js
@@ -1,24 +1,19 @@
 'use strict'
 
-const mysql = require('mysql')
-const config = require('../configuration')
 const escapeStringForSql = require('./escapeStringForSql')
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: config.db.userName,
-  password: config.db.passWord,
-  database: 'beob'
-})
 
 module.exports = (request, callback) => {
   const tabelle = escapeStringForSql(request.params.tabelle) // der Name der Tabelle, aus der die Daten geholt werden sollen
   const feld = escapeStringForSql(request.params.feld) // das ist der Name des Feldes, das verglichen wird
   const wert = escapeStringForSql(request.params.wert) // der Wert im Feld, das verglichen wird
 
-  connection.query(`
-    SELECT *
-    FROM ${tabelle}
-    WHERE ${feld} = "${wert}"`,
-    (err, data) => callback(err, data)
+  request.pg.client.query(`
+    SELECT
+      *
+    FROM
+      beob.${tabelle}
+    WHERE
+      "${feld}" = '${wert}'`,
+    (err, data) => callback(err, data.rows)
   )
 }
