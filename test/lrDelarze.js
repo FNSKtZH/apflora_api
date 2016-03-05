28ea8bfddb7f1252a1e0215d8bd82ee660a2199f
Alexander Gabriel <alex.barbalex@gmail.com>
2016-03-05 20:24:15 +0100

    test lrDelarze and popChKarte

---
+++ test/lrDelarze.js
@@ -0,0 +1,31 @@
+'use strict'
+
+// Load modules
+
+const Code = require('code')
+const Lab = require('lab')
+
+// test shortcuts
+
+const lab = exports.lab = Lab.script()
+const describe = lab.describe
+const it = lab.it
+const expect = Code.expect
+
+// start server
+
+const server = require('../server.js')
+
+// test
+
+describe('/lrDelarze', () => {
+  it('should get more than 250 rows', (done) => {
+    const method = 'GET'
+    const url = '/lrDelarze'
+    server.inject({ method, url }, (res) => {
+      expect(res.statusCode).to.equal(200)
+      expect(res.result.length).to.be.above(250)
+      done()
+    })
+  })
+})
---
+++ test/popChKarte.js
@@ -0,0 +1,31 @@
+'use strict'
+
+// Load modules
+
+const Code = require('code')
+const Lab = require('lab')
+
+// test shortcuts
+
+const lab = exports.lab = Lab.script()
+const describe = lab.describe
+const it = lab.it
+const expect = Code.expect
+
+// start server
+
+const server = require('../server.js')
+
+// test
+
+describe('/popChKarte', () => {
+  it('should get 1 row for popId -2141970766', (done) => {
+    const method = 'GET'
+    const url = '/popChKarte/popId=-2141970766'
+    server.inject({ method, url }, (res) => {
+      expect(res.statusCode).to.equal(200)
+      expect(res.result.length).to.equal(1)
+      done()
+    })
+  })
+})
