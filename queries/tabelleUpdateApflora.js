dab0c0974e775d402e9d15cbb4f726c66def0a76
Alexander Gabriel <alex@gabriel-software.ch>
2016-03-05 10:49:02 +0100

    convert to pg

--- queries/tabelleUpdateApflora.js
+++ queries/tabelleUpdateApflora.js
@@ -5,16 +5,9 @@
 
 'use strict'
 
-const mysql = require('mysql')
 const _ = require('lodash')
 const config = require('../configuration')
 const escapeStringForSql = require('./escapeStringForSql')
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: config.db.userName,
-  password: config.db.passWord,
-  database: 'apflora'
-})
 
 module.exports = (request, callback) => {
   const tabelle = escapeStringForSql(request.params.tabelle) // der Name der Tabelle, in der die Daten gespeichert werden sollen
@@ -24,29 +17,33 @@ module.exports = (request, callback) => {
   const wert = escapeStringForSql(request.params.wert) // der Wert, der gespeichert werden soll
   const user = escapeStringForSql(request.params.user) // der Benutzername
   const date = new Date().toISOString() // wann gespeichert wird
-  const table = _.find(config.tables, {tabelleInDb: tabelle}) // Infos über die Tabelle holen
+  const table = _.find(config.tables, { tabelleInDb: tabelle }) // Infos über die Tabelle holen
   const mutWannFeld = table.mutWannFeld // so heisst das Feld für MutWann
   const mutWerFeld = table.mutWerFeld // so heisst das Feld für MutWer
   let sql = `
-    UPDATE ${tabelle}
+    UPDATE
+      apflora.${tabelle}
     SET
-      ${feld}="${wert}",
-      ${mutWannFeld}="${date}",
-      ${mutWerFeld}="${user}"
-    WHERE ${tabelleIdFeld} = "${tabelleId}"`
+      "${feld}" = '${wert}',
+      "${mutWannFeld}" = '${date}',
+      "${mutWerFeld}" = '${user}'
+    WHERE
+      "${tabelleIdFeld}" = '${tabelleId}'`
   // Ist ein Feld neu leer, muss NULL übergeben werden. wert ist dann 'undefined'
   if (!wert) {
     sql = `
-      UPDATE ${tabelle}
+      UPDATE
+        apflora.${tabelle}
       SET
-        ${feld}= NULL,
-        ${mutWannFeld}="${date}",
-        ${mutWerFeld}="${user}"
-      WHERE ${tabelleIdFeld} = "${tabelleId}"`
+        "${feld}" = NULL,
+        "${mutWannFeld}" = '${date}',
+        "${mutWerFeld}" = '${user}'
+      WHERE
+        "${tabelleIdFeld}" = '${tabelleId}'`
   }
 
-  connection.query(
+  request.pg.client.query(
     sql,
-    (err, data) => callback(err, data)
+    (err, data) => callback(err, data.rows)
   )
 }
