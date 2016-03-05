4d2c9599ad1a699ef402ad292c06f317882c3252
Alexander Gabriel <alex@gabriel-software.ch>
2016-03-05 10:32:14 +0100

    convert to pg

--- queries/tabelleDeleteApflora.js
+++ queries/tabelleDeleteApflora.js
@@ -1,29 +1,19 @@
 'use strict'
 
-const pg = require('pg')
-const config = require('../configuration')
-const connectionString = config.pg.connectionString
 const escapeStringForSql = require('./escapeStringForSql')
 
 module.exports = (request, callback) => {
   const tabelle = escapeStringForSql(request.params.tabelle) // der Name der Tabelle, aus der die Daten gelöscht werden sollen
   const tabelleIdFeld = escapeStringForSql(request.params.tabelleIdFeld) // das ist der Name der ID der Tabelle
   const tabelleId = escapeStringForSql(request.params.tabelleId) // der Wert der ID des zu löschenden Datensatzes
+  const sql = `
+    DELETE FROM
+      apflora.${tabelle}
+    WHERE
+      "${tabelleIdFeld}" = '${tabelleId}'`
 
-  // get a pg client from the connection pool
-  pg.connect(connectionString, (error, apfDb, done) => {
-    if (error) {
-      if (apfDb) done(apfDb)
-      console.log('an error occured when trying to connect to db apflora')
-    }
-    const sql = `
-      DELETE FROM
-        apflora.${tabelle}
-      WHERE
-        "${tabelleIdFeld}" = '${tabelleId}'`
-    apfDb.query(sql, (error, result) => {
-      if (error) callback(error, null)
-      callback(error, result.rows)
-    })
+  request.pg.client.query(sql, (error, result) => {
+    if (error) callback(error, null)
+    callback(error, result.rows)
   })
 }
