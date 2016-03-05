0691b887a2b190165231f45c377b5b7403c87036
Alexander Gabriel <alex@gabriel-software.ch>
2016-03-05 10:31:03 +0100

    convert to pg

--- queries/qkView.js
+++ queries/qkView.js
@@ -1,14 +1,6 @@
 'use strict'
 
-const mysql = require('mysql')
-const config = require('../configuration')
 const escapeStringForSql = require('./escapeStringForSql')
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: config.db.userName,
-  password: config.db.passWord,
-  database: 'apflora_views'
-})
 
 module.exports = (request, callback) => {
   const viewName = escapeStringForSql(request.params.viewName)
@@ -21,19 +13,24 @@ module.exports = (request, callback) => {
     // if berichtjahr was passed, get only data of that year
     sql = `
       SELECT *
-      FROM ${viewName}
-      WHERE ApArtId = ${apId}
-        AND Berichtjahr = ${berichtjahr}`
+      FROM
+        views.${viewName}
+      WHERE
+        "ApArtId" = ${apId}
+        AND "Berichtjahr" = ${berichtjahr}`
   } else {
     sql = `
-      SELECT *
-      FROM ${viewName}
-      WHERE ApArtId = ${apId}`
+      SELECT
+        *
+      FROM
+        views.${viewName}
+      WHERE
+        "ApArtId" = ${apId}`
   }
 
   // Daten abfragen
-  connection.query(
+  request.pg.client.query(
     sql,
-    (err, data) => callback(err, data)
+    (err, data) => callback(err, data.rows)
   )
 }
