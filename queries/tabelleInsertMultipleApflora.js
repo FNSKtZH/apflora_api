cff3063b631735589bd33f2932cef99ea5199734
Alexander Gabriel <alex@gabriel-software.ch>
2016-03-05 10:41:23 +0100

    convert to pg

--- queries/tabelleInsertMultipleApflora.js
+++ queries/tabelleInsertMultipleApflora.js
@@ -5,35 +5,34 @@
 
 'use strict'
 
-const mysql = require('mysql')
 const _ = require('lodash')
 const config = require('../configuration')
 const escapeStringForSql = require('./escapeStringForSql')
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: config.db.userName,
-  password: config.db.passWord,
-  database: 'apflora'
-})
 
 module.exports = (request, callback) => {
   const tabelle = escapeStringForSql(request.params.tabelle) // der Name der Tabelle, in der die Daten gespeichert werden sollen
   // NICHT escapeStringForSql anwenden
+  const configTable = _.find(config.tables, {tabelleInDb: tabelle}) // die table in der Konfiguration, welche die Informationen dieser Tabelle enthält
+  const tabelleIdFeld = configTable.tabelleIdFeld
   let felder = request.params.felder // Ein Objekt mit allen feldern und deren Werten des wiederherzustellenden Datensatzes
 
   felder = JSON.parse(felder)
 
   // Feldnamen und -werte extrahieren
-  const feldnamen = Object.keys(felder).join()
-  const feldwerte = _.values(felder).join('","')
+  const feldnamen = Object.keys(felder).join('","')
+  const feldwerte = _.values(felder).join("','")
 
   // sql beginnen
   const sql = `
-    INSERT INTO ${tabelle} (${feldnamen})
-    VALUES ("${feldwerte}")`
+    INSERT INTO
+      ${tabelle} ("${feldnamen}")
+    VALUES
+      ("${feldwerte}")
+    RETURNING
+      ${tabelle}."${tabelleIdFeld}"`
 
-  connection.query(
+  request.pg.client.query(
     sql,
-    (err, data) => callback(err, data.insertId)
+    (err, data) => callback(err, data)
   )
 }
