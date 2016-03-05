05a1b305122de374682808a5c9750f99cf8ba9b7
Alexander Gabriel <alex@gabriel-software.ch>
2016-03-05 10:43:24 +0100

    convert to pg

--- queries/tabelleSelectApfloraString.js
+++ queries/tabelleSelectApfloraString.js
@@ -1,28 +1,18 @@
 'use strict'
 
-const pg = require('pg')
-const config = require('../configuration')
 const escapeStringForSql = require('./escapeStringForSql')
-const connectionString = config.pg.connectionString
 
 module.exports = (request, callback) => {
   const tabelle = escapeStringForSql(request.params.tabelle) // der Name der Tabelle, aus der die Daten geholt werden sollen
   const feld = escapeStringForSql(request.params.feld) // das ist der Name des Feldes, das verglichen wird
   const wert = escapeStringForSql(request.params.wert) // der Wert im Feld, das verglichen wird
+  const sql = `
+    SELECT
+      *
+    FROM
+      apflora.${tabelle}
+    WHERE
+      "${feld}" = '${wert}'`
 
-  // get a pg client from the connection pool
-  pg.connect(connectionString, (error, apfDb, done) => {
-    if (error) {
-      if (apfDb) done(apfDb)
-      console.log('an error occured when trying to connect to db apflora')
-    }
-    const sql = `
-      SELECT
-        *
-      FROM
-        apflora.${tabelle}
-      WHERE
-        "${feld}" = '${wert}'`
-    apfDb.query(sql, (error, result) => callback(error, result.rows))
-  })
+  request.pg.client.query(sql, (error, result) => callback(error, result.rows))
 }
