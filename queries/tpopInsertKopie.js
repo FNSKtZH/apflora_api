ead5ab5de8be10896c571fd845edb367f4a16479
Alexander Gabriel <alex@gabriel-software.ch>
2016-03-05 11:00:00 +0100

    convert to pg

--- queries/tpopInsertKopie.js
+++ queries/tpopInsertKopie.js
@@ -1,15 +1,7 @@
 'use strict'
 
-const mysql = require('mysql')
 const async = require('async')
-const config = require('../configuration')
 const escapeStringForSql = require('./escapeStringForSql')
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: config.db.userName,
-  password: config.db.passWord,
-  database: 'apflora'
-})
 
 module.exports = (request, callback) => {
   const tpopId = escapeStringForSql(request.params.tpopId)
@@ -21,42 +13,52 @@ module.exports = (request, callback) => {
     [
       (callback) => {
         // Temporäre Tabelle erstellen mit dem zu kopierenden Datensatz
-        connection.query(
-          `DROP TABLE IF EXISTS tmp`,
+        request.pg.client.query(
+          'DROP TABLE IF EXISTS tmp',
           // nur allfällige Fehler weiterleiten
           (err) => callback(err, null)
         )
       },
       (callback) => {
         // Temporäre Tabelle erstellen mit dem zu kopierenden Datensatz
-        connection.query(`
-          CREATE TEMPORARY TABLE tmp
-          SELECT *
-          FROM tpop
-          WHERE TPopId = ${tpopId}`,
+        request.pg.client.query(`
+          CREATE TEMPORARY TABLE
+            tmp
+          SELECT
+            *
+          FROM
+            tpop
+          WHERE
+            "TPopId" = ${tpopId}`,
           // nur allfällige Fehler weiterleiten
           (err) => callback(err, null)
         )
       },
       (callback) => {
         // TPopId anpassen
-        connection.query(`
-          UPDATE tmp
+        request.pg.client.query(`
+          UPDATE
+            tmp
           SET
-            TPopId = NULL,
-            PopId = ${popId},
-            MutWann = "${date}",
-            MutWer = "${user}"`,
+            "TPopId" = NULL,
+            "PopId" = ${popId},
+            "MutWann" = '${date}',
+            "MutWer" = '${user}'`,
           // nur allfällige Fehler weiterleiten
           (err) => callback(err, null)
         )
       },
       (callback) => {
-        connection.query(`
-          INSERT INTO tpop
-          SELECT *
-          FROM tmp`,
-          (err, data) => callback(err, data.insertId)
+        request.pg.client.query(`
+          INSERT INTO
+            tpop
+          SELECT
+            *
+          FROM
+            tmp
+          RETURNING
+            tpop."TPopId"`,
+          (err, data) => callback(err, data)
         )
       }
     ],
