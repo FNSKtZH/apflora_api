816f48d73a3915f638681c1367f58774789293ae
Alexander Gabriel <alex@gabriel-software.ch>
2016-03-05 16:13:44 +0100

    convert to pg

--- queries/tpopMassnTypen.js
+++ queries/tpopMassnTypen.js
@@ -1,24 +1,14 @@
 'use strict'
 
-const pg = require('pg')
-const config = require('../configuration')
-const connectionString = config.pg.connectionString
-
 module.exports = (request, callback) => {
-  // get a pg client from the connection pool
-  pg.connect(connectionString, (error, apfDb, done) => {
-    if (error) {
-      if (apfDb) done(apfDb)
-      console.log('an error occured when trying to connect to db apflora')
-    }
-    const sql = `
-      SELECT
-        "MassnTypCode" as id,
-        "MassnTypTxt"
-      FROM
-        apflora.tpopmassn_typ_werte
-      ORDER BY
-        "MassnTypOrd"`
-    apfDb.query(sql, (error, result) => callback(error, result.rows))
-  })
+  const sql = `
+    SELECT
+      "MassnTypCode" as id,
+      "MassnTypTxt"
+    FROM
+      apflora.tpopmassn_typ_werte
+    ORDER BY
+      "MassnTypOrd"`
+
+  request.pg.client.query(sql, (error, result) => callback(error, result.rows))
 }
