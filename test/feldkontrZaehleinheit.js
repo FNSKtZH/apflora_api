6921f039465b78e15175d8bc8a7581d4df66883f
Alexander Gabriel <alex.barbalex@gmail.com>
2016-03-05 20:14:39 +0100

    test feldkontrZaehleinheit

---
+++ test/feldkontrZaehleinheit.js
@@ -0,0 +1,31 @@
+'use strict'
+
+// Load modules
+
+const Code = require('code')
+const Lab = require('lab')
+
+// test shortcuts
+
+const lab = exports.lab = Lab.script()
+const describe = lab.describe
+const it = lab.it
+const expect = Code.expect
+
+// start server
+
+const server = require('../server.js')
+
+// test
+
+describe('/feldkontrZaehleinheit', () => {
+  it('should get more than 20 Zähleinheiten', (done) => {
+    const method = 'GET'
+    const url = '/feldkontrZaehleinheit'
+    server.inject({ method, url }, (res) => {
+      expect(res.statusCode).to.equal(200)
+      expect(res.result.length).to.be.above(20)
+      done()
+    })
+  })
+})
