31d0ab5870a756025f3a23c20e8ab8da03156d3a
Alexander Gabriel <alex.barbalex@gmail.com>
2016-03-05 20:16:59 +0100

    test gemeinden

---
+++ test/gemeinden.js
@@ -0,0 +1,31 @@
+'use strict'
+
+// Load modules
+
+const Code = require('code')
+const Lab = require('lab')
+
+// test shortcuts
+
+const lab = exports.lab = Lab.script()
+const describe = lab.describe
+const it = lab.it
+const expect = Code.expect
+
+// start server
+
+const server = require('../server.js')
+
+// test
+
+describe('/gemeinden', () => {
+  it('should get more than 150 Gemeinden', (done) => {
+    const method = 'GET'
+    const url = '/gemeinden'
+    server.inject({ method, url }, (res) => {
+      expect(res.statusCode).to.equal(200)
+      expect(res.result.length).to.be.above(150)
+      done()
+    })
+  })
+})
