88b914b7df4d43ed5b278a9722561ffd459057a7
Alexander Gabriel <alex.barbalex@gmail.com>
2016-03-05 20:05:19 +0100

    optimize code

--- queries/feldkontrInsert.js
+++ queries/feldkontrInsert.js
@@ -8,16 +8,24 @@ module.exports = (request, callback) => {
   const user = escapeStringForSql(request.params.user) // der Benutzername
   const date = new Date().toISOString() // wann gespeichert wird
   let sql = `
-    INSERT INTO apflora.tpopkontr ("TPopId", "MutWann", "MutWer")
-    VALUES (${tpopId}, '${date}', '${user}')
-    RETURNING tpopkontr."TPopKontrId"`
+    INSERT INTO
+      apflora.tpopkontr
+      ("TPopId", "MutWann", "MutWer")
+    VALUES
+      (${tpopId}, '${date}', '${user}')
+    RETURNING
+      tpopkontr."TPopKontrId"`
 
   // sql schreiben
   if (tpopKontrtyp === 'tpopfreiwkontr') {
     sql = `
-      INSERT INTO apflora.tpopkontr ("TPopId", "TPopKontrTyp", "MutWann", "MutWer")
-      VALUES (${tpopId}, 'Freiwilligen-Erfolgskontrolle', '${date}', '${user}')
-      RETURNING tpopkontr."TPopKontrId"`
+      INSERT INTO
+        apflora.tpopkontr
+        ("TPopId", "TPopKontrTyp", "MutWann", "MutWer")
+      VALUES
+        (${tpopId}, 'Freiwilligen-Erfolgskontrolle', '${date}', '${user}')
+      RETURNING
+        tpopkontr."TPopKontrId"`
   }
   request.pg.client.query(sql, (error, result) => callback(error, result))
 }
