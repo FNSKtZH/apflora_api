5e2f844a687692c1ee68cd13d954166de2395524
Alexander Gabriel <alex.barbalex@gmail.com>
2016-03-06 21:04:52 +0100

    test qkView

---
+++ test/qkView.js
@@ -0,0 +1,27 @@
+'use strict'
+
+// load modules
+const Code = require('code')
+const Lab = require('lab')
+
+// shortcuts
+const lab = exports.lab = Lab.script()
+const describe = lab.describe
+const it = lab.it
+const expect = Code.expect
+
+// get server
+const server = require('../server.js')
+
+// test
+describe('/qkView', () => {
+  it('should get status o.k. for view v_qk_pop_ohnepopnr, apId = 900 and berichtjahr = 2015', (done) => {
+    const method = 'GET'
+    const url = '/qkView/v_qk_pop_ohnepopnr/900'
+    server.inject({ method, url }, (res) => {
+      expect(res.statusCode).to.equal(200)
+      expect(res.result.length).to.be.at.least(0)
+      done()
+    })
+  })
+})
