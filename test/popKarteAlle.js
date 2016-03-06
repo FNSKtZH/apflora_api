441302a5d7ab9d0725f48800bb1bc849c314b4cb
Alexander Gabriel <alex.barbalex@gmail.com>
2016-03-06 18:27:14 +0100

    test popKarteAlle

---
+++ test/popKarteAlle.js
@@ -0,0 +1,27 @@
+'use strict'
+
+// load modules
+const Code = require('code')
+const Lab = require('lab')
+
+// shortcuts
+const lab = exports.lab = Lab.script()
+const describe = lab.describe
+const it = lab.it
+const expect = Code.expect
+
+// get server
+const server = require('../server.js')
+
+// test
+describe('/popKarteAlle', () => {
+  it('should get more than 50 rows for apId = 900', (done) => {
+    const method = 'GET'
+    const url = '/popKarteAlle/apId=900'
+    server.inject({ method, url }, (res) => {
+      expect(res.statusCode).to.equal(200)
+      expect(res.result.length).to.be.above(50)
+      done()
+    })
+  })
+})
