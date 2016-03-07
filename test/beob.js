1bbee2f76e5adcbead0cb2ba7ce93e6e41905277
Alexander Gabriel <alex@gabriel-software.ch>
2016-03-07 16:17:26 +0100

    test beob/tabelle

---
+++ test/beob.js
@@ -0,0 +1,36 @@
+'use strict'
+
+// Load modules
+const Code = require('code')
+const Lab = require('lab')
+
+// test shortcuts
+const lab = exports.lab = Lab.script()
+const describe = lab.describe
+const it = lab.it
+const expect = Code.expect
+
+// get server
+const server = require('../server.js')
+
+// test
+describe('/apflora', () => {
+  it('should get one row from adb_eigenschaften with TaxonomieId 100', (done) => {
+    const method = 'GET'
+    const url = '/beob/tabelle=adb_eigenschaften/feld=TaxonomieId/wertNumber=100'
+    server.inject({ method, url }, (res) => {
+      expect(res.statusCode).to.equal(200)
+      expect(res.result.length).to.equal(1)
+      done()
+    })
+  })
+  it('should get one row from adb_eigenschaften with Artname "Affodill"', (done) => {
+    const method = 'GET'
+    const url = '/beob/tabelle=adb_eigenschaften/feld=NameDeutsch/wertString=Affodill'
+    server.inject({ method, url }, (res) => {
+      expect(res.statusCode).to.equal(200)
+      expect(res.result.length).to.equal(1)
+      done()
+    })
+  })
+})
