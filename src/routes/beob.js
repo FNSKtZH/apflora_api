91850e8cf0c88c8d048e46dedc111db8a9d39f69
Alexander Gabriel <alex@gabriel-software.ch>
2016-03-07 16:24:04 +0100

    remove unused api

--- src/routes/beob.js
+++ src/routes/beob.js
@@ -1,8 +1,14 @@
 'use strict'
 
+const queryTabelleSelectBeobNumber = require('../../queries/tabelleSelectBeobNumber.js')
 const queryTabelleSelectBeobString = require('../../queries/tabelleSelectBeobString.js')
 
 module.exports = [
+  {
+    method: 'GET',
+    path: '/beob/tabelle={tabelle}/feld={feld}/wertNumber={wert}',
+    handler: queryTabelleSelectBeobNumber
+  },
   {
     method: 'GET',
     path: '/beob/tabelle={tabelle}/feld={feld}/wertString={wert}',
