822c9bc752a158ba40bcb36ca120159ba94674ee
Alexander Gabriel <alex@gabriel-software.ch>
2016-03-07 17:41:28 +0100

    test tpopKarte

---
+++ test/tpopKarte.js
@@ -0,0 +1,27 @@
+'use strict'
+
+// load modules
+const Code = require('code')
+const Lab = require('lab')
+
+// shortcuts
+const lab = exports.lab = Lab.script()
+const describe = lab.describe
+const it = lab.it
+const expect = Code.expect
+
+// get server
+const server = require('../server.js')
+
+// test
+describe('/tpopKarte', () => {
+  it('should get 0 or one rows for TPopId = 13542', (done) => {
+    const method = 'GET'
+    const url = '/tpopKarte/tpopId=13542'
+    server.inject({ method, url }, (res) => {
+      expect(res.statusCode).to.equal(200)
+      expect(res.result.length).to.be.at.least(0)
+      done()
+    })
+  })
+})
