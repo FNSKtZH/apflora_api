e496b9edbbc2522cdfae647a46af04fa7db440bd
Alexander Gabriel <alex@gabriel-software.ch>
2016-03-08 11:40:59 +0100

    optimize code

--- server.js
+++ server.js
@@ -6,6 +6,8 @@
 
 const Hapi = require('hapi')
 const Inert = require('inert')
+const dbConnection = require('./dbConnection.js')
+const pgPlugin = require('./pgPlugin.js')
 // wird nur in Entwicklung genutzt
 // in new Hapi.Server() einsetzen
 const serverOptionsDevelopment = {
@@ -15,15 +17,16 @@ const serverOptionsDevelopment = {
   }
 }
 const server = new Hapi.Server(serverOptionsDevelopment)
+
+server.connection(dbConnection)
+
 // non-Query routes hat to be separated
 // because when testing directory handler produces an error
 const routes = require('./src/routes').concat(require('./src/nonQueryRoutes'))
 
-server.connection(require('./dbConnection.js'))
-
 server.register(Inert, (err) => {
   if (err) console.log('failed loading Inert plugin')
-  server.register(require('./pgPlugin.js'), (error) => {
+  server.register(pgPlugin, (error) => {
     if (error) console.log('failed loading pg plugin')
     // add all the routes
     server.route(routes)
