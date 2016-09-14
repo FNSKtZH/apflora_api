7bc2b5817d99a8652c515a4bdffa92926409a011
Alexander Gabriel <alex@gabriel-software.ch>
2016-09-14 18:18:21 +0200

    refactor to use pg-promise

--- queries/node/projekt.js
+++ queries/node/projekt.js
@@ -2,7 +2,6 @@
 /* eslint-disable no-console */
 
 const app = require('ampersand-app')
-const parallel = require('async/parallel')
 const rootNode = require('../../src/rootNode')
 
 const sqlProjListe = `
@@ -39,43 +38,37 @@ module.exports = (request, callback) => {
   const id = encodeURIComponent(request.params.id)
   const user = 23
 
-  // const projektListe = 
-
-  parallel({
-    projektListe(cb) {
-      app.db.many(sqlProjListe, user)
-        .then((projects) => {
-          const nodes = projects.map(projekt => ({
-            nodeId: `projekt/${projekt.ProjId}`,
-            datasetId: projekt.ProjId,
-            type: 'dataset',
-            name: projekt.ProjName,
-            expanded: id && id === projekt.ProjId,
-            nrOfUnloadedChildren: 'todo',
-          }))
-          cb(null, nodes)
-        })
-        .catch(error => cb(error, null))
-    },
-    anzApListe(cb) {
-      app.db.many(sqlAnzApListe)
-        .then(apListe => cb(null, apListe))
-        .catch(error => cb(error, null))
-    },
-  }, (err, results) => {
-    if (err) return callback(err, null)
-
-    const projektListe = results.projektListe || []
-    const anzApListe = results.anzApListe
-
-    projektListe.forEach((node) => {
-      const nrOfChildrenRow = anzApListe.find(el => el.ProjId === node.datasetId)
-      const nrOfChildren = nrOfChildrenRow.anzAp || 0
-      node.nrOfUnloadedChildren = nrOfChildren
+  const getProjektListe = app.db.many(sqlProjListe, user)
+    .then((projects) => {
+      const nodes = projects.map(projekt => ({
+        nodeId: `projekt/${projekt.ProjId}`,
+        datasetId: projekt.ProjId,
+        type: 'dataset',
+        name: projekt.ProjName,
+        expanded: id && id === projekt.ProjId,
+        nrOfUnloadedChildren: 'todo',
+      }))
+      return nodes
+    })
+    .catch((error) => {
+      throw error
+    })
+  const getAnzApListe = app.db.many(sqlAnzApListe)
+    .then(apListe =>
+      apListe || []
+    )
+    .catch((error) => {
+      throw error
     })
 
-    projektListe.unshift(rootNode)
-
-    callback(null, projektListe)
-  })
+  Promise.all([getProjektListe, getAnzApListe])
+    .then(([projektListe, anzApListe]) => {
+      projektListe.forEach((node) => {
+        const nrOfChildrenRow = anzApListe.find(el => el.ProjId === node.datasetId)
+        const nrOfChildren = nrOfChildrenRow.anzAp || 0
+        node.nrOfUnloadedChildren = nrOfChildren
+      })
+      projektListe.unshift(rootNode)
+      callback(null, projektListe)
+    })
 }
