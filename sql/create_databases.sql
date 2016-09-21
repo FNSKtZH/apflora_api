37088dd00c3958214f21c2d2f5b10e1dfc699789
Alexander Gabriel <alex@gabriel-software.ch>
2016-09-21 14:05:53 +0200

    create user reader

--- sql/create_databases.sql
+++ sql/create_databases.sql
@@ -3,3 +3,7 @@ CREATE SCHEMA apflora;
 CREATE SCHEMA views;
 CREATE SCHEMA beob;
 CREATE EXTENSION pgcrypto;
+
+-- create role reader in pgAdmin, then:
+GRANT USAGE ON SCHEMA apflora TO read;
+GRANT SELECT ON ALL TABLES IN SCHEMA apflora TO read;
