cec7a6c9bbe5df6fd6aa36761eac338a1d5799ed
Alexander Gabriel <alex@gabriel-software.ch>
2016-09-21 18:41:41 +0200

    optimize

--- queries/escapeStringForSql.js
+++ queries/escapeStringForSql.js
@@ -1,31 +1,28 @@
 'use strict'
 
 module.exports = (str) => {
-  if (str) {
-    if (typeof str === `string`) {
-      return str.replace(/[\0\x08\x09\x1a\n\r"'\\\%]/g, (char) => {
-        switch (char) {
-          case `\0`:
-            return `\\0`
-          case `\x08`:
-            return `\\b`
-          case `\x09`:
-            return `\\t`
-          case `\x1a`:
-            return `\\z`
-          case `\n`:
-            return `\\n`
-          case `\r`:
-            return `\\r`
-          case `"`:
-          case `'`:
-          case `\\`:
-          case `%`:
-            return `\\${char}` // prepends a backslash to backslash, percent and double/single quotes
-        }
-      })
-    } else {
-      return str
-    }
+  if (str && typeof str === `string`) {
+    return str.replace(/[\0\x08\x09\x1a\n\r"'\\\%]/g, (char) => {
+      switch (char) {
+        case `\0`:
+          return `\\0`
+        case `\x08`:
+          return `\\b`
+        case `\x09`:
+          return `\\t`
+        case `\x1a`:
+          return `\\z`
+        case `\n`:
+          return `\\n`
+        case `\r`:
+          return `\\r`
+        case `"`:
+        case `'`:
+        case `\\`:
+        case `%`:
+          return `\\${char}` // prepends a backslash to backslash, percent and double/single quotes
+      }
+    })
   }
+  return str
 }
