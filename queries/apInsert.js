c263678fe6e8c370618601beda61b46bb8ce82a2
Alexander Gabriel <alex@gabriel-software.ch>
2016-09-21 12:42:45 +0200

    promisify

--- queries/apInsert.js
+++ queries/apInsert.js
@@ -2,54 +2,48 @@
 
 const app = require('ampersand-app')
 
-/*
- * need to test this with postgresql
- */
-
-const escapeStringForSql = require('./escapeStringForSql')
-
 module.exports = (request, callback) => {
-  const apId = escapeStringForSql(request.params.apId)
-  const user = escapeStringForSql(request.params.user)
+  const apId = encodeURIComponent(request.params.apId)
+  const user = encodeURIComponent(request.params.user)
   const date = new Date().toISOString()
 
-  // neuen AP einfügen
-  request.pg.client.query(`
-    INSERT INTO
-      apflora.ap ("ApArtId", "MutWann", "MutWer")
-    VALUES
-      (${apId}, '${date}', '${user}')
-    ON CONFLICT DO NOTHING`,
-    (err, data) => {
-      if (err) callback(err, null)
-      // Artwert holen
-      request.pg.client.query(`
-        SELECT
-          "Artwert"
-        FROM
-          beob.adb_eigenschaften
-        WHERE
-          "TaxonomieId" = ${apId}`,
-        (err, data) => {
-          // keine Fehler melden, wenn bloss der Artwert nicht geholt wurde
-          if (data && data[0]) {
-            const artwert = data[0]
-            if (artwert) {
-              request.pg.client.query(`
-                UPDATE
-                  apflora.ap
-                SET
-                  "ApArtwert" = '${artwert}'
-                WHERE
-                  "ApArtId" = ${apId}`,
-                (err, data) => callback(err, apId)
-              )
-            }
-          } else {
-            callback(err, null)
-          }
-        }
-      )
-    }
-  )
+  app.db.task(function*(t) {
+    // neuen AP einfügen
+    const sqlInsert = `
+      INSERT INTO
+        apflora.ap ("ApArtId", "MutWann", "MutWer")
+      VALUES
+        (${apId}, '${date}', '${user}')
+      ON CONFLICT DO NOTHING`
+    yield app.db.none(sqlInsert)
+
+    // Artwert holen
+    const sqlGet = `
+      SELECT
+        "Artwert"
+      FROM
+        beob.adb_eigenschaften
+      WHERE
+        "TaxonomieId" = ${apId}`
+    return yield app.db.oneOrNone(sqlGet)
+  })
+    .then((row) => {
+      // keine Fehler melden, wenn bloss der Artwert nicht geholt wurde
+      if (row && row.Artwert) {
+        request.pg.client.query(`
+          UPDATE
+            apflora.ap
+          SET
+            "ApArtwert" = '${row.Artwert}'
+          WHERE
+            "ApArtId" = ${apId}`,
+          (err, data) => callback(err, apId)
+        )
+      } else {
+        callback(null, apId)
+      }
+    })
+    .catch((error) =>
+      callback(error, null)
+    )
 }
