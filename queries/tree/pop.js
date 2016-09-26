457b96ac8432ba77e6aa0f22a7afb9c710fa20eb
Alexander Gabriel <alex@gabriel-software.ch>
2016-09-26 08:25:30 +0200

    modularize

--- queries/tree/pop.js
+++ queries/tree/pop.js
@@ -6,29 +6,7 @@ const escapeStringForSql = require(`../escapeStringForSql`)
 const erstelleTpopOrdner = require(`./tpopOrdner`)
 const erstellePopMassnBerOrdner = require(`./popMassnBerOrdner`)
 const erstellePopBerOrdner = require(`./popBerOrdner`)
-
-// erhält die höchste PopNr der Liste und die aktuelle
-// stellt der aktuellen PopNr soviele Nullen voran, dass
-// alle Nummern dieselbe Anzahl stellen haben
-const ergaenzePopNrUmFuehrendeNullen = (popNrMaxPassed, popNrPassed) => {
-  /* jslint white: true, plusplus: true */
-  if (!popNrPassed && popNrPassed !== 0) return null
-  if (!popNrMaxPassed && popNrMaxPassed !== 0) return null
-
-  // Nummern in Strings umwandeln
-  const popNrMax = popNrMaxPassed.toString()
-  let popNr = popNrPassed.toString()
-
-  let stellendifferenz = popNrMax.length - popNr.length
-
-  // Voranzustellende Nullen generieren
-  while (stellendifferenz > 0) {
-    popNr = `0${popNr}`
-    stellendifferenz -= 1
-  }
-
-  return popNr
-}
+const ergaenzePopNrUmFuehrendeNullen = require(`../../src/ergaenzePopNrUmFuehrendeNullen`)
 
 module.exports = (request, reply) => {
   const apId = escapeStringForSql(request.params.apId)
