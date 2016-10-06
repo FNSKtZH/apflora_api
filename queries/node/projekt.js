764e862220d1d2f5fda4471100404ad2a8d68940
Alexander Gabriel <alex@gabriel-software.ch>
2016-10-06 22:44:45 +0200

    work on nodes

--- queries/node/projekt.js
+++ queries/node/projekt.js
@@ -12,80 +12,81 @@ module.exports = (request, callback) => {
     id = parseInt(id, 0)
   }
 
-  app.db.task(function* getData() {
-    const projektListe = yield app.db.many(`
-      SELECT
-        "ProjId",
-        "ProjName",
-        (
-          SELECT
-            COUNT(*)
-          FROM
-            apflora.ap
-          WHERE
-            apflora.ap."ProjId" = apflora.projekt."ProjId"
-        ) AS "AnzAp",
-        (
-          SELECT
-            COUNT(*)
-          FROM
-            apflora.apberuebersicht
-          WHERE
-            apflora.apberuebersicht."ProjId" = apflora.projekt."ProjId"
-        ) AS "AnzApberuebersicht"
-      FROM
-        apflora.projekt
-      WHERE
-        "ProjId" IN (
-          SELECT
-            "ProjId"
-          FROM
-            apflora.userprojekt
-          WHERE
-            "UserId" = $1
-        )
-      ORDER BY
-        "ProjName"
-      `,
-      user
+
+  app.db.many(`
+    SELECT
+      "ProjId",
+      "ProjName",
+      (
+        SELECT
+          COUNT(*)
+        FROM
+          apflora.ap
+        WHERE
+          apflora.ap."ProjId" = apflora.projekt."ProjId"
+      ) AS "AnzAp",
+      (
+        SELECT
+          COUNT(*)
+        FROM
+          apflora.apberuebersicht
+        WHERE
+          apflora.apberuebersicht."ProjId" = apflora.projekt."ProjId"
+      ) AS "AnzApberuebersicht"
+    FROM
+      apflora.projekt
+    WHERE
+      "ProjId" IN (
+        SELECT
+          "ProjId"
+        FROM
+          apflora.userprojekt
+        WHERE
+          "UserId" = $1
+      )
+    ORDER BY
+      "ProjName"
+    `,
+    user
+  )
+    .then(projektListe =>
+      projektListe.map((projekt) => {
+        const idActive = !!id && id === projekt.ProjId
+        // const oneProject = projektListe.length === 1  // temporarily disabled
+        return {
+          nodeId: `projekt/${projekt.ProjId}`,
+          table: `projekt`,
+          id: projekt.ProjId,
+          name: projekt.ProjName,
+          expanded: idActive, // || oneProject,  // temporarily disabled
+          path: [`Projekte`, projekt.ProjId],
+          children: [
+            // ap folder
+            {
+              nodeId: `projekt/${projekt.ProjId}/ap`,
+              folder: `ap`,
+              table: `projekt`,
+              id: projekt.ProjId,
+              name: `Arten (${projekt.AnzAp})`,
+              expanded: false,
+              children: [0],
+              path: [`Projekte`, projekt.ProjId, `Arten`]
+            },
+            // apberuebersicht folder
+            {
+              nodeId: `projekt/${projekt.ProjId}/apberuebersicht`,
+              folder: `apberuebersicht`,
+              table: `projekt`,
+              id: projekt.ProjId,
+              name: `AP-Berichte (${projekt.AnzApberuebersicht})`,
+              expanded: false,
+              children: [0],
+              path: [`Projekte`, projekt.ProjId, `AP-Berichte`]
+            },
+          ],
+        }
+      })
     )
-    return projektListe.map((projekt) => {
-      const idActive = !!id && id === projekt.ProjId
-      // const oneProject = projektListe.length === 1  // temporarily disabled
-      return {
-        nodeId: `projekt/${projekt.ProjId}`,
-        table: `projekt`,
-        id: projekt.ProjId,
-        name: projekt.ProjName,
-        expanded: idActive, // || oneProject,  // temporarily disabled
-        path: [`Projekte`, projekt.ProjId],
-        children: [
-          // ap folder
-          {
-            nodeId: `projekt/${projekt.ProjId}/ap`,
-            folder: `ap`,
-            table: `projekt`,
-            id: projekt.ProjId,
-            name: `Arten (${projekt.AnzAp})`,
-            expanded: false,
-            children: [0],
-            path: [`Projekte`, projekt.ProjId, `Arten`]
-          },
-          // apberuebersicht folder
-          {
-            nodeId: `projekt/${projekt.ProjId}/apberuebersicht`,
-            folder: `apberuebersicht`,
-            table: `projekt`,
-            id: projekt.ProjId,
-            name: `AP-Berichte (${projekt.AnzApberuebersicht})`,
-            expanded: false,
-            children: [0],
-            path: [`Projekte`, projekt.ProjId, `AP-Berichte`]
-          },
-        ],
-      }
-    })
-  })
     .then(nodes => callback(null, nodes))
     .catch(error => callback(error, null))
 }
