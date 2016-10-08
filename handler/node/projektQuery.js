ed7f745c096f2f40b1d781ba6c0aa687073ac65e
Alexander Gabriel <alex.barbalex@gmail.com>
2016-10-08 14:59:49 +0200

    work on loading all arten

--- handler/node/projektQuery.js
+++ handler/node/projektQuery.js
@@ -5,85 +5,89 @@
 const app = require(`ampersand-app`)
 const apFolderQuery = require(`./apFolderQuery`)
 
-// TODO: get real user
+let apFolder = [0]
+let projektListe
 
 module.exports = ({ user, projId, withAp, withApBerUebersicht }) =>
-  app.db.any(`
-    SELECT
-      "ProjId",
-      "ProjName",
-      (
-        SELECT
-          COUNT(*)
-        FROM
-          apflora.ap
-        WHERE
-          apflora.ap."ProjId" = apflora.projekt."ProjId"
-      ) AS "AnzAp",
-      (
-        SELECT
-          COUNT(*)
-        FROM
-          apflora.apberuebersicht
-        WHERE
-          apflora.apberuebersicht."ProjId" = apflora.projekt."ProjId"
-      ) AS "AnzApberuebersicht"
-    FROM
-      apflora.projekt
-    WHERE
-      "ProjId" IN (
-        SELECT
-          "ProjId"
-        FROM
-          apflora.userprojekt
-        WHERE
-          "UserId" = $1
-      )
-    ORDER BY
-      "ProjName"
-    `,
-    user
-  )
-    .then(projektListe =>
-      projektListe.map((projekt) => {
-        const idActive = !!projId && projId === projekt.ProjId
-        // const oneProject = projektListe.length === 1  // temporarily disabled
-        return {
-          nodeId: `projekt/${projekt.ProjId}`,
-          table: `projekt`,
-          id: projekt.ProjId,
-          name: projekt.ProjName,
-          expanded: idActive, // || oneProject,  // temporarily disabled
-          urlPath: [`Projekte`, projekt.ProjId],
-          nodeIdPath: [`projekt/${projekt.ProjId}`],
-          children: [
-            // ap folder
-            {
-              nodeId: `projekt/${projekt.ProjId}/ap`,
-              folder: `ap`,
-              table: `projekt`,
-              id: projekt.ProjId,
-              name: `Arten (${projekt.AnzAp})`,
-              expanded: false,
-              children: [0],
-              urlPath: [`Projekte`, projekt.ProjId, `Arten`],
-              nodeIdPath: [`projekt/${projekt.ProjId}`, `projekt/${projekt.ProjId}/ap`],
-            },
-            // apberuebersicht folder
-            {
-              nodeId: `projekt/${projekt.ProjId}/apberuebersicht`,
-              folder: `apberuebersicht`,
-              table: `projekt`,
-              id: projekt.ProjId,
-              name: `AP-Berichte (${projekt.AnzApberuebersicht})`,
-              expanded: false,
-              children: [0],
-              urlPath: [`Projekte`, projekt.ProjId, `AP-Berichte`],
-              nodeIdPath: [`projekt/${projekt.ProjId}`, `projekt/${projekt.ProjId}/apberuebersicht`],
-            },
-          ],
-        }
-      })
+  app.db.task(function* getData() {
+    if (withAp) {
+      apFolder = yield apFolderQuery(projId)
+    }
+    projektListe = yield app.db.any(`
+      SELECT
+        "ProjId",
+        "ProjName",
+        (
+          SELECT
+            COUNT(*)
+          FROM
+            apflora.ap
+          WHERE
+            apflora.ap."ProjId" = apflora.projekt."ProjId"
+        ) AS "AnzAp",
+        (
+          SELECT
+            COUNT(*)
+          FROM
+            apflora.apberuebersicht
+          WHERE
+            apflora.apberuebersicht."ProjId" = apflora.projekt."ProjId"
+        ) AS "AnzApberuebersicht"
+      FROM
+        apflora.projekt
+      WHERE
+        "ProjId" IN (
+          SELECT
+            "ProjId"
+          FROM
+            apflora.userprojekt
+          WHERE
+            "UserId" = $1
+        )
+      ORDER BY
+        "ProjName"
+      `,
+      user
     )
+    return projektListe.map((projekt) => {
+      const idActive = !!projId && projId === projekt.ProjId
+      // const oneProject = projektListe.length === 1  // temporarily disabled
+      return {
+        nodeId: `projekt/${projekt.ProjId}`,
+        table: `projekt`,
+        id: projekt.ProjId,
+        name: projekt.ProjName,
+        expanded: idActive, // || oneProject,  // temporarily disabled
+        urlPath: [`Projekte`, projekt.ProjId],
+        nodeIdPath: [`projekt/${projekt.ProjId}`],
+        children: [
+          // ap folder
+          {
+            nodeId: `projekt/${projekt.ProjId}/ap`,
+            folder: `ap`,
+            table: `projekt`,
+            id: projekt.ProjId,
+            name: `Arten (${projekt.AnzAp})`,
+            expanded: false,
+            children: apFolder,
+            urlPath: [`Projekte`, projekt.ProjId, `Arten`],
+            nodeIdPath: [`projekt/${projekt.ProjId}`, `projekt/${projekt.ProjId}/ap`],
+          },
+          // apberuebersicht folder
+          {
+            nodeId: `projekt/${projekt.ProjId}/apberuebersicht`,
+            folder: `apberuebersicht`,
+            table: `projekt`,
+            id: projekt.ProjId,
+            name: `AP-Berichte (${projekt.AnzApberuebersicht})`,
+            expanded: false,
+            children: [0],
+            urlPath: [`Projekte`, projekt.ProjId, `AP-Berichte`],
+            nodeIdPath: [`projekt/${projekt.ProjId}`, `projekt/${projekt.ProjId}/apberuebersicht`],
+          },
+        ],
+      }
+    })
+  })
     .then(nodes => nodes)
     .catch((error) => { throw error })
--- queries/node/apFolder.js
+++ queries/node/apFolder.js
@@ -1,15 +1,22 @@
 'use strict'
 
 const apFolderQuery = require(`./apFolderQuery`)
+const projektQuery = require(`./projektQuery`)
 
 module.exports = (request, callback) => {
   let id = encodeURIComponent(request.query.id)
+  const levels = encodeURIComponent(request.query.levels)
 
   if (id) {
     id = parseInt(id, 0)
   }
-
-  apFolderQuery(id)
-    .then(nodes => callback(null, nodes))
-    .catch(error => callback(error, null))
+  console.log(`levels:`, levels)
+  if (levels && levels === `all`) {
+    const user = 23
+    projektQuery({ user, projId: id, withAp: true, withApBerUebersicht: false })
+  } else {
+    apFolderQuery(id)
+      .then(nodes => callback(null, nodes))
+      .catch(error => callback(error, null))
+  }
 }
