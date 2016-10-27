8f556e08d8418d0149c3473da1f829342479a4d2
Alexander Gabriel <alex@gabriel-software.ch>
2016-10-27 13:01:12 +0200

    validate

--- src/routes/apfloraPostMultiple.js
+++ src/routes/apfloraPostMultiple.js
@@ -1,11 +1,20 @@
 'use strict'
 
+const Joi = require(`joi`)
 const queryTabelleInsertMultipleApflora = require(`../../handler/tabelleInsertMultipleApflora.js`)
 
 module.exports = [
   {
     method: `POST`,
     path: `/insertMultiple/apflora/tabelle={tabelle}/felder={felder}`,
-    handler: queryTabelleInsertMultipleApflora
+    handler: queryTabelleInsertMultipleApflora,
+    config: {
+      validate: {
+        params: {
+          tabelle: Joi.string().required(),
+          felder: Joi.any().required(),
+        }
+      }
+    }
   }
 ]
--- src/routes/apfloraPutMultiple.js
+++ src/routes/apfloraPutMultiple.js
@@ -1,11 +1,20 @@
 'use strict'
 
+const Joi = require(`joi`)
 const queryTabelleUpdateMultipleApflora = require(`../../handler/tabelleUpdateMultipleApflora.js`)
 
 module.exports = [
   {
     method: `PUT`,
     path: `/updateMultiple/apflora/tabelle={tabelle}/felder={felder}`,
-    handler: queryTabelleUpdateMultipleApflora
+    handler: queryTabelleUpdateMultipleApflora,
+    config: {
+      validate: {
+        params: {
+          tabelle: Joi.string().required(),
+          felder: Joi.any().required(),
+        }
+      }
+    }
   }
 ]
