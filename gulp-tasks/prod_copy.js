fc9517c93acfa01cbeeb9b4f4b840fb7e995afd3
Alexander Gabriel <alex@gabriel-software.ch>
2016-11-03 15:15:25 +0100

    lint

--- gulp-tasks/prod_copy.js
+++ gulp-tasks/prod_copy.js
@@ -3,7 +3,7 @@
 var gulp = require('gulp')
 var requireDir = require('require-dir')
 
-requireDir('../gulp-tasks', {recurse: true})
+requireDir('../gulp-tasks', { recurse: true })
 
 gulp.task(
   'prod_copy',
