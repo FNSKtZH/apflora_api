61225f17bbbbccb5c33787f56c4d8ac112902e69
Alexander Gabriel <alex@gabriel-software.ch>
2016-11-10 22:27:10 +0100

    get to work on localhost

--- dbConnection.js
+++ dbConnection.js
@@ -2,11 +2,16 @@
 
 module.exports = () => {
   const isDev = process.env.NODE_ENV !== `production`
+  const options = {
+    host: `0.0.0.0`,
+    port: 4001,
   }
+
   if (isDev) {
     options.routes = {
       cors: true,
     }
   }
+
   return options
 }
