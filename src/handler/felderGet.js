c78c2b1362132793f2164f91263eeff14a3f9d07
Alexander Gabriel <alex@gabriel-software.ch>
2016-11-11 22:39:46 +0100

    add felder route

--- src/handler/felderGet.js
+++ src/handler/felderGet.js
@@ -2,6 +2,22 @@
 
 const app = require(`ampersand-app`)
 
+const sql = `
+  SELECT
+    table_schema,
+    table_name,
+    column_name,
+    data_type,
+    character_maximum_length
+  FROM
+    information_schema.columns
+  WHERE
+    table_schema in ('apflora', 'beob')
+  ORDER BY
+    table_schema,
+    table_name,
+    column_name`
+
 module.exports = (request, callback) =>
   app.db.any(sql)
     .then(rows => callback(null, rows))
