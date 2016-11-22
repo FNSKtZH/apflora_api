a190b05261537f7ff58e2a4241834185d9805378
Alexander Gabriel <alex@gabriel-software.ch>
2016-11-23 00:35:05 +0100

    work on references

--- sql/references.sql
+++ sql/references.sql
@@ -18,36 +18,6 @@ REFERENCES apflora.adresse ("AdrId")
 ON DELETE SET NULL
 ON UPDATE CASCADE;
 
-ALTER TABLE apflora.apber
-DROP CONSTRAINT IF EXISTS apber_fk_ap;
-
-ALTER TABLE apflora.apber
-ADD CONSTRAINT apber_fk_ap
-FOREIGN KEY ("ApArtId")
-REFERENCES apflora.ap
-ON DELETE SET NULL
-ON UPDATE CASCADE;
-
-ALTER TABLE apflora.ber
-DROP CONSTRAINT IF EXISTS ber_fk_ap;
-
-ALTER TABLE apflora.ber
-ADD CONSTRAINT ber_fk_ap
-FOREIGN KEY ("ApArtId")
-REFERENCES apflora.ap
-ON DELETE SET NULL
-ON UPDATE CASCADE;
-
-ALTER TABLE apflora.erfkrit
-DROP CONSTRAINT IF EXISTS erfkrit_fk_ap;
-
-ALTER TABLE apflora.erfkrit
-ADD CONSTRAINT erfkrit_fk_ap
-FOREIGN KEY ("ApArtId")
-REFERENCES apflora.ap
-ON DELETE SET NULL
-ON UPDATE CASCADE;
-
 ALTER TABLE apflora.ap
 DROP CONSTRAINT IF EXISTS ap_fk_ap_bearbstand_werte;
 
@@ -65,6 +35,16 @@ ALTER TABLE apflora.ap
 ADD CONSTRAINT ap_fk_ap_umsetzung_werte
 FOREIGN KEY ("ApUmsetzung")
 REFERENCES apflora.ap_umsetzung_werte ("DomainCode")
+ON DELETE SET NULL
+ON UPDATE CASCADE;
+
+ALTER TABLE apflora.apber
+DROP CONSTRAINT IF EXISTS apber_fk_ap;
+
+ALTER TABLE apflora.apber
+ADD CONSTRAINT apber_fk_ap
+FOREIGN KEY ("ApArtId")
+REFERENCES apflora.ap
 ON DELETE SET NULL
 ON UPDATE CASCADE;
 
@@ -88,5 +68,64 @@ ALTER TABLE apflora.apber
 ADD CONSTRAINT apber_fk_adresse
 FOREIGN KEY ("JBerBearb")
 REFERENCES apflora.adresse ("AdrId")
+ON DELETE SET NULL
+ON UPDATE CASCADE;
+
+ALTER TABLE apflora.ber
+DROP CONSTRAINT IF EXISTS ber_fk_ap;
+
+ALTER TABLE apflora.ber
+ADD CONSTRAINT ber_fk_ap
+FOREIGN KEY ("ApArtId")
+REFERENCES apflora.ap
+ON DELETE SET NULL
+ON UPDATE CASCADE;
+
+ALTER TABLE apflora.erfkrit
+DROP CONSTRAINT IF EXISTS erfkrit_fk_ap;
+
+ALTER TABLE apflora.erfkrit
+ADD CONSTRAINT erfkrit_fk_ap
+FOREIGN KEY ("ApArtId")
+REFERENCES apflora.ap
+ON DELETE SET NULL
+ON UPDATE CASCADE;
+
+ALTER TABLE apflora.erfkrit
+DROP CONSTRAINT IF EXISTS erfkrit_fk_ap_erfkrit_werte;
+
+ALTER TABLE apflora.erfkrit
+ADD CONSTRAINT erfkrit_fk_ap_erfkrit_werte
+FOREIGN KEY ("ErfkritErreichungsgrad")
+REFERENCES apflora.ap_erfkrit_werte ("BeurteilId")
+ON DELETE SET NULL
+ON UPDATE CASCADE;
+
+-- assozart
+ALTER TABLE apflora.assozart
+DROP CONSTRAINT IF EXISTS assozart_fk_ap;
+
+ALTER TABLE apflora.assozart
+ADD CONSTRAINT assozart_fk_ap
+FOREIGN KEY ("AaApArtId")
+REFERENCES apflora.ap
+ON DELETE SET NULL
+ON UPDATE CASCADE;
+
+-- add unique constraint to beob.adb_eigenschaften.TaxonomieId
+ALTER TABLE beob.adb_eigenschaften
+ADD UNIQUE ("TaxonomieId");
+
+-- get rid of 0 values
+-- update beob.adb_eigenschaften set "TaxonomieId" = NULL where "TaxonomieId" = 0;
+update apflora.assozart set "AaSisfNr" = NULL where "AaSisfNr" = 0;
+
+ALTER TABLE apflora.assozart
+DROP CONSTRAINT IF EXISTS assozart_fk_adb_eigenschaften;
+
+ALTER TABLE apflora.assozart
+ADD CONSTRAINT assozart_fk_adb_eigenschaften
+FOREIGN KEY ("AaSisfNr")
+REFERENCES beob.adb_eigenschaften ("TaxonomieId")
 ON DELETE SET NULL
 ON UPDATE CASCADE;
