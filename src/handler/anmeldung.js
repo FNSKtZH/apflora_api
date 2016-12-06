8d43bfdf0b5d821064071dac144837729d7c1127
Alexander Gabriel <alex@gabriel-software.ch>
2016-12-06 09:47:08 +0100

    dont encode user name

--- src/handler/anmeldung.js
+++ src/handler/anmeldung.js
@@ -3,8 +3,11 @@
 const app = require(`ampersand-app`)
 
 module.exports = (request, callback) => {
-  const userName = encodeURIComponent(request.params.name)
+  const userName = request.params.name
   const password = encodeURIComponent(request.params.pwd)
+  console.log(`request.params.name:`, request.params.name)
+  console.log(`userName:`, userName)
+  console.log(`password:`, password)
   const sql = `
     SELECT
       "NurLesen"
@@ -15,6 +18,9 @@ module.exports = (request, callback) => {
       AND "Passwort" = $2`
 
   app.db.any(sql, [userName, password])
-    .then(rows => callback(null, rows))
+    .then((rows) => {
+      console.log(`rows:`, rows)
+      callback(null, rows)
+    })
     .catch(error => callback(error, null))
 }
