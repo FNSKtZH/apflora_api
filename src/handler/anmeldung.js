685f923fe5f612377cf33c765e10d06bcc7915c5
Alexander Gabriel <alex@gabriel-software.ch>
2016-12-06 09:49:25 +0100

    clean up

--- src/handler/anmeldung.js
+++ src/handler/anmeldung.js
@@ -5,9 +5,6 @@ const app = require(`ampersand-app`)
 module.exports = (request, callback) => {
   const userName = request.params.name
   const password = encodeURIComponent(request.params.pwd)
-  console.log(`request.params.name:`, request.params.name)
-  console.log(`userName:`, userName)
-  console.log(`password:`, password)
   const sql = `
     SELECT
       "NurLesen"
@@ -19,7 +16,6 @@ module.exports = (request, callback) => {
 
   app.db.any(sql, [userName, password])
     .then((rows) => {
-      console.log(`rows:`, rows)
       callback(null, rows)
     })
     .catch(error => callback(error, null))
