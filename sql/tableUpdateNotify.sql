eee7c8b29db503fc9dbea09f89f80057748591b5
Alexander Gabriel <alex@gabriel-software.ch>
2016-12-08 11:08:13 +0100

    work on pubsub

--- sql/tableUpdateNotify.sql
+++ sql/tableUpdateNotify.sql
@@ -1,15 +1,22 @@
+CREATE OR REPLACE FUNCTION tpop_update_notify() RETURNS trigger AS $$
 DECLARE
   id bigint;
 BEGIN
   IF TG_OP = 'INSERT' OR TG_OP = 'UPDATE' THEN
+    id = NEW."TPopId";
   ELSE
+    id = OLD."TPopId";
   END IF;
+  PERFORM pg_notify('tpop_update', row_to_json(NEW)::text);
   RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
 DROP TRIGGER tpop_notify_update ON apflora.tpop;
+CREATE TRIGGER tpop_notify_update AFTER UPDATE ON apflora.tpop FOR EACH ROW EXECUTE PROCEDURE tpop_update_notify();
 
 DROP TRIGGER tpop_notify_insert ON apflora.tpop;
+CREATE TRIGGER tpop_notify_insert AFTER INSERT ON apflora.tpop FOR EACH ROW EXECUTE PROCEDURE tpop_update_notify();
 
 DROP TRIGGER tpop_notify_delete ON apflora.tpop;
+CREATE TRIGGER tpop_notify_delete AFTER DELETE ON apflora.tpop FOR EACH ROW EXECUTE PROCEDURE tpop_update_notify();
