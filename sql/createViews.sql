98cdd40b36d44f667a098967e5aa916e40b251d7
Alexander Gabriel <alex@gabriel-software.ch>
2016-12-15 17:29:30 +0100

    add qk views

--- sql/createViews.sql
+++ sql/createViews.sql
@@ -8902,6 +8902,96 @@ WHERE
       apflora.tpop."TPopHerkunft" = 100
   );
 
+DROP VIEW IF EXISTS views.v_qk_pop_statusansaatversuchalletpoperloschen CASCADE;
+CREATE OR REPLACE VIEW views.v_qk_pop_statusansaatversuchalletpoperloschen AS
+SELECT DISTINCT
+  apflora.pop."ApArtId",
+  'Population: Status ist "angesiedelt, Ansaatversuch", alle Teilpopulationen sind gemäss Status erloschen:'::text AS "hw",
+  concat(
+    '<a href="http://apflora.ch/index.html?ap=',
+    apflora.pop."ApArtId",
+    '&pop=',
+    apflora.pop."PopId",
+    '" target="_blank">',
+    COALESCE(
+      concat('Pop: ', apflora.pop."PopNr"),
+      concat('Pop: id=', apflora.pop."PopId")
+    ),
+    '</a>'
+  ) AS "link"
+FROM
+  apflora.pop
+    INNER JOIN apflora.tpop
+    ON apflora.tpop."PopId" = apflora.pop."PopId"
+WHERE
+  apflora.pop."PopHerkunft" = 201
+  AND EXISTS (
+    SELECT
+      1
+    FROM
+      apflora.tpop
+    WHERE
+      apflora.tpop."TPopHerkunft" IN (101, 202, 211)
+      AND apflora.tpop."PopId" = apflora.pop."PopId"
+  )
+  AND NOT EXISTS (
+    SELECT
+      1
+    FROM
+      apflora.tpop
+    WHERE
+      apflora.tpop."TPopHerkunft" NOT IN (101, 202, 211)
+      AND apflora.tpop."PopId" = apflora.pop."PopId"
+  )
+ORDER BY
+  apflora.pop."ApArtId",
+  concat(
+    '<a href="http://apflora.ch/index.html?ap=',
+    apflora.pop."ApArtId",
+    '&pop=',
+    apflora.pop."PopId",
+    '" target="_blank">',
+    COALESCE(
+      concat('Pop: ', apflora.pop."PopNr"),
+      concat('Pop: id=', apflora.pop."PopId")
+    ),
+    '</a>'
+  );
+
+DROP VIEW IF EXISTS views.v_qk2_pop_statusansaatversuchalletpoperloschen CASCADE;
+CREATE OR REPLACE VIEW views.v_qk2_pop_statusansaatversuchalletpoperloschen AS
+SELECT DISTINCT
+  apflora.ap."ProjId",
+  apflora.pop."ApArtId",
+  'Population: Status ist "angesiedelt, Ansaatversuch", alle Teilpopulationen sind gemäss Status erloschen:'::text AS "hw",
+  ARRAY['Projekte', 1 , 'Arten', apflora.ap."ApArtId", 'Populationen', apflora.pop."PopId"]::text[] AS "url"
+FROM
+  apflora.ap
+    INNER JOIN apflora.pop
+      INNER JOIN apflora.tpop
+      ON apflora.tpop."PopId" = apflora.pop."PopId"
+    ON apflora.pop."ApArtId" = apflora.ap."ApArtId"
+WHERE
+  apflora.pop."PopHerkunft" = 201
+  AND EXISTS (
+    SELECT
+      1
+    FROM
+      apflora.tpop
+    WHERE
+      apflora.tpop."TPopHerkunft" IN (101, 202, 211)
+      AND apflora.tpop."PopId" = apflora.pop."PopId"
+  )
+  AND NOT EXISTS (
+    SELECT
+      1
+    FROM
+      apflora.tpop
+    WHERE
+      apflora.tpop."TPopHerkunft" NOT IN (101, 202, 211)
+      AND apflora.tpop."PopId" = apflora.pop."PopId"
+  );
+
 DROP VIEW IF EXISTS views.v_qk_pop_statusansaatversuchmittpopursprerloschen CASCADE;
 CREATE OR REPLACE VIEW views.v_qk_pop_statusansaatversuchmittpopursprerloschen AS
 SELECT DISTINCT
