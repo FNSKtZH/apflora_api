3a4c5f5d11a7c967f75cb7cbf692352816fc5efe
Alexander Gabriel <alex@gabriel-software.ch>
2017-02-11 13:39:41 +0100

    Use faster query to make test pass

--- test/tree.js
+++ test/tree.js
@@ -15,9 +15,9 @@ const server = require(`../server.js`)
 
 // test
 describe(`/tree`, () => {
-  it(`should get more than 1 row for popId = 5495`, (done) => {
+  it(`should get status code 200 for apId 100`, (done) => {
     const method = `GET`
-    const url = `/tree/apId=900`
+    const url = `/tree/apId=100`
     server.injectThen({ method, url })
       .then((res) => {
         expect(res.statusCode).to.equal(200)
