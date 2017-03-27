fa49645187f9f6ec0c2545bc52098bce3eec7942
Alexander Gabriel <alex@gabriel-software.ch>
2017-03-27 15:39:07 +0200

    optimize

--- src/handler/exportView.js
+++ src/handler/exportView.js
@@ -6,7 +6,11 @@ const escapeStringForSql = require(`../escapeStringForSql`)
 module.exports = (request, callback) => {
   const view = escapeStringForSql(request.params.view) // Name des Views, aus dem die Daten geholt werden sollen
   const apId = escapeStringForSql(request.params.apId)
-  const sql = apId ? `SELECT * FROM views.${view} WHERE "ApArtId" = ${apId}` : `SELECT * FROM views.${view}`
+  const sql = (
+    apId ?
+    `SELECT * FROM views.${view} WHERE "ApArtId" = ${apId}` :
+    `SELECT * FROM views.${view}`
+  )
 
   app.db.any(sql)
     .then((rows) => {
