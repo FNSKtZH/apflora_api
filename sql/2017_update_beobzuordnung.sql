29c4359c6b9aefe117afb5142973c3bb3e70bdbb
Alexander Gabriel <alex@gabriel-software.ch>
2017-04-21 17:16:29 +0200

    update beobzuordnung

---
+++ sql/2017_update_beobzuordnung.sql
@@ -0,0 +1,39 @@
+-- select original id
+SELECT
+  beob.id,
+  beob."IdField",
+  beob.data->>(SELECT "IdField" FROM beob.beob WHERE id = beob2.id) AS "OriginalId"
+FROM
+  beob.beob
+  INNER JOIN beob.beob AS beob2
+  ON beob2.id = beob.id
+LIMIT 10
+
+-- update beobid in beobzuordnung
+-- this query runs forever
+UPDATE apflora.beobzuordnung
+SET "BeobId" = (
+  SELECT
+    beob.id
+  FROM
+    beob.beob
+    INNER JOIN beob.beob AS beob2
+    ON beob2.id = beob.id
+  WHERE
+    beob.data->>(SELECT "IdField" FROM beob.beob WHERE id = beob2.id) = apflora.beobzuordnung."NO_NOTE"
+)
+
+-- check result
+SELECT
+  beobzuordnung."BeobId",
+  beobzuordnung."NO_NOTE",
+  beob.id,
+  beob."QuelleId",
+  beob.data->>(SELECT "IdField" FROM beob.beob WHERE id = beob2.id) AS "OriginalId"
+FROM
+  apflora.beobzuordnung
+  LEFT JOIN beob.beob
+    INNER JOIN beob.beob AS beob2
+    ON beob2.id = beob.id
+  ON apflora.beobzuordnung."BeobId" = beob.beob.id
+LIMIT 10
