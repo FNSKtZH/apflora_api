d339bff1381aa9e5a3b400a92e2737ac329c60d4
Alexander Gabriel <alex@gabriel-software.ch>
2017-05-09 10:56:13 +0200

    remove unneeded

--- sql/2017_update_beobzuordnung.sql
+++ sql/2017_update_beobzuordnung.sql
@@ -9,9 +9,6 @@ FROM
   ON beob2.id = beob.id
 LIMIT 10
 
-CREATE INDEX ON beob.beob((data->>'NO_NOTE'));
-CREATE INDEX ON beob.beob((data->>'NO_NOTE_PROJET'));
-
 -- update beobid in beobzuordnung
 -- this query took 2.46 hours!
 UPDATE apflora.beobzuordnung
