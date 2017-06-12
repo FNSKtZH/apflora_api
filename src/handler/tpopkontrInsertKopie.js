a211bc220a262974d959d6c23a21db90b1f440d7
Alexander Gabriel <alex@gabriel-software.ch>
2017-06-12 16:08:36 +0200

    correct for guids

--- src/handler/tpopkontrInsertKopie.js
+++ src/handler/tpopkontrInsertKopie.js
@@ -8,22 +8,23 @@ module.exports = (request, callback) => {
   const tpopId = escapeStringForSql(request.params.tpopId)
   const tpopKontrId = escapeStringForSql(request.params.tpopKontrId)
   const user = escapeStringForSql(request.params.user) // der Benutzername
-  const date = new Date().toISOString()                // wann gespeichert wird
+  const date = new Date().toISOString() // wann gespeichert wird
   let newTPopKontrId = null
 
-  app.db.tx(function* manageData() {
-    // need a statement that selects all fields but some
-    // from: http://dba.stackexchange.com/questions/1957/sql-select-all-columns-except-some
-    const fieldsListStatement = yield app.db.one(`
+  app.db
+    .tx(function* manageData() {
+      // need a statement that selects all fields but some
+      // from: http://dba.stackexchange.com/questions/1957/sql-select-all-columns-except-some
+      const fieldsListStatement = yield app.db.one(`
       SELECT array_to_string(ARRAY(SELECT '"' || c.column_name || '"'
         FROM information_schema.columns As c
           WHERE table_name = 'tpopkontr'
-          AND  c.column_name NOT IN('TPopKontrId', 'TPopKontrGuid')
+          AND  c.column_name NOT IN('TPopKontrId', 'TPopKontrGuid', 'ZeitGuid', 'TPopKontrGuid_alt', 'ZeitGuid_alt')
         ), ',') As sqlstmt
     `)
-    const fieldsList = fieldsListStatement.sqlstmt
+      const fieldsList = fieldsListStatement.sqlstmt
 
-    newTPopKontrId = yield app.db.one(`
+      newTPopKontrId = yield app.db.one(`
       INSERT INTO
         apflora.tpopkontr (${fieldsList})
       SELECT
@@ -35,9 +36,9 @@ module.exports = (request, callback) => {
       RETURNING "TPopKontrId"
     `)
 
-    newTPopKontrId = newTPopKontrId.TPopKontrId
+      newTPopKontrId = newTPopKontrId.TPopKontrId
 
-    yield app.db.none(`
+      yield app.db.none(`
       UPDATE
         apflora.tpopkontr
       SET
@@ -49,8 +50,8 @@ module.exports = (request, callback) => {
         "TPopKontrId" = ${newTPopKontrId}
     `)
 
-    // Zählungen der herkunfts-Kontrolle holen und der neuen Kontrolle anfügen
-    return yield app.db.none(`
+      // Zählungen der herkunfts-Kontrolle holen und der neuen Kontrolle anfügen
+      return yield app.db.none(`
       INSERT INTO
         apflora.tpopkontrzaehl
         (
@@ -71,9 +72,8 @@ module.exports = (request, callback) => {
       FROM
         apflora.tpopkontrzaehl
       WHERE
-        apflora.tpopkontrzaehl."TPopKontrId" = ${tpopKontrId}`
-    )
-  })
+        apflora.tpopkontrzaehl."TPopKontrId" = ${tpopKontrId}`)
+    })
     .then(() => callback(null, newTPopKontrId))
     .catch(error => callback(error, null))
 }
--- src/handler/tpopmassnInsertKopie.js
+++ src/handler/tpopmassnInsertKopie.js
@@ -11,19 +11,20 @@ module.exports = (request, callback) => {
   const date = new Date().toISOString() // wann gespeichert wird
   let newTPopMassnId = null
 
-  app.db.tx(function* manageData() {
-    // need a statement that selects all fields but some
-    // from: http://dba.stackexchange.com/questions/1957/sql-select-all-columns-except-some
-    const fieldsListStatement = yield app.db.one(`
+  app.db
+    .tx(function* manageData() {
+      // need a statement that selects all fields but some
+      // from: http://dba.stackexchange.com/questions/1957/sql-select-all-columns-except-some
+      const fieldsListStatement = yield app.db.one(`
       SELECT array_to_string(ARRAY(SELECT '"' || c.column_name || '"'
         FROM information_schema.columns As c
           WHERE table_name = 'tpopmassn'
-          AND  c.column_name NOT IN('TPopMassnId', 'TPopMassnGuid')
+          AND  c.column_name NOT IN('TPopMassnId', 'TPopMassnGuid', 'TPopMassnGuid_alt')
         ), ',') As sqlstmt
     `)
-    const fieldsList = fieldsListStatement.sqlstmt
+      const fieldsList = fieldsListStatement.sqlstmt
 
-    newTPopMassnId = yield app.db.one(`
+      newTPopMassnId = yield app.db.one(`
       INSERT INTO
         apflora.tpopmassn (${fieldsList})
       SELECT
@@ -35,9 +36,9 @@ module.exports = (request, callback) => {
       RETURNING "TPopMassnId"
     `)
 
-    newTPopMassnId = newTPopMassnId.TPopMassnId
+      newTPopMassnId = newTPopMassnId.TPopMassnId
 
-    yield app.db.none(`
+      yield app.db.none(`
       UPDATE
         apflora.tpopmassn
       SET
@@ -48,7 +49,7 @@ module.exports = (request, callback) => {
       WHERE
         "TPopMassnId" = ${newTPopMassnId}
     `)
-  })
+    })
     // neue id zurück liefern
     .then(() => callback(null, newTPopMassnId))
     .catch(error => callback(error, null))
